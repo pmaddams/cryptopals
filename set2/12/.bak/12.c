5036af1e7a3e4edb9107b1634603d47590107c40
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-01-06 15:25:46 -0500

    set2

---
+++ set2/12/.bak/12.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include <openssl/bio.h>
+#include <openssl/evp.h>
+
+const char secret[] =
+    "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg"
+    "aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq"
+    "dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg"
+    "YnkK";
--- set2/12/12.c
+++ set2/12/12.c
@@ -1,4 +1,6 @@
+#include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <openssl/bio.h>
 #include <openssl/evp.h>
@@ -8,3 +10,50 @@ const char secret[] =
     "aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq"
     "dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg"
     "YnkK";
+
+int
+encrypt(FILE *in, FILE *out)
+{
+	static char key[16];
+	static BIO *cip;
+	BIO *bio;
+	char buf[BUFSIZ];
+	int nr;
+
+	while (*key == '\0')
+		arc4random_buf(key, 16);
+
+	if (cip == NULL) {
+		if ((cip = BIO_new(BIO_f_cipher())) == NULL)
+			goto fail;
+		BIO_set_cipher(cip, EVP_aes_128_ecb(), key, NULL, 1);
+	}
+
+	if ((bio = BIO_new_fp(in, BIO_NOCLOSE)) == NULL)
+		goto fail;
+
+	BIO_push(cip, bio);
+
+	while ((nr = BIO_read(cip, buf, BUFSIZ)) > 0)
+		if (fwrite(buf, nr, 1, out) < 1)
+			goto fail;
+
+	BIO_pop(bio);
+	BIO_free(bio);
+
+	return 1;
+fail:
+	return 0;
+}
+
+size_t
+crack_blksiz(void)
+{
+	return 0;
+}
+
+int
+main(void)
+{
+	return 0;
+}
