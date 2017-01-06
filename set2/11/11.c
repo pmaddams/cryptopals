49b0fbc190d03f0dbc7eeda5e4d01bb655ba4021
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-01-06 01:14:20 -0500

    set2

--- set2/11/11.c
+++ set2/11/11.c
@@ -6,8 +6,10 @@
 #include <openssl/bio.h>
 #include <openssl/evp.h>
 
-#define KEY	"YELLOW SUBMARINE"
 #define BLKSIZ	16
+
+#define ECB	0
+#define CBC	1
 
 char *
 extend(char *buf, size_t *lenp)
@@ -44,7 +46,7 @@ encrypt(FILE *in, FILE *out, uint8_t *key, int *modep)
 	    (cip = BIO_new(BIO_f_cipher())) == NULL)
 		goto fail;
 
-	cipher = (*modep = arc4random_uniform(2)) ?
+	cipher = (*modep = arc4random_uniform(2)) == ECB ?
 	    EVP_aes_128_ecb() : EVP_aes_128_cbc();
 
 	BIO_set_cipher(cip, cipher, key, NULL, 1);
@@ -54,6 +56,8 @@ encrypt(FILE *in, FILE *out, uint8_t *key, int *modep)
 		if (fwrite(buf, nr, 1, out) < 1)
 			goto fail;
 
+	BIO_free_all(bio);
+
 	return 1;
 fail:
 	return 0;
@@ -62,18 +66,28 @@ fail:
 int
 main(void)
 {
-	char *buf;
-	size_t len;
+	char key[BLKSIZ], *buf, *outbuf;
+	size_t len, outlen;
+	FILE *in, *out;
+	int realmode, guessmode;
+
+	arc4random_buf(key, BLKSIZ);
 
 	len = BLKSIZ*3;
 	if ((buf = malloc(len+1)) == NULL)
 		err(1, NULL);
 	memset(buf, 'A', len);
 
-	if ((buf = extend(buf, &len)) == NULL)
+	if ((buf = extend(buf, &len)) == NULL ||
+	    (in = fmemopen(buf, len, "r")) == NULL ||
+	    (out = open_memstream(&outbuf, &outlen)) == NULL ||
+	    (encrypt(in, out, key, &realmode)) == 0)
 		err(1, NULL);
+	fclose(out);
 
-	puts(buf);
+	guessmode = memcmp(outbuf+BLKSIZ, outbuf+BLKSIZ*2, BLKSIZ) == 0 ? ECB : CBC;
+
+	puts(realmode == guessmode ? "success" : "failure");
 
 	exit(0);
 }
