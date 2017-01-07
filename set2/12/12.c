2e91f286b0e0bfa6368330bf319c17a687f80b32
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-01-06 19:11:51 -0500

    set2

--- set2/12/12.c
+++ set2/12/12.c
@@ -1,11 +1,10 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include <openssl/bio.h>
 #include <openssl/evp.h>
-
-#define HASHSIZ 127
 
 struct entry {
 	uint8_t *blk;
@@ -19,7 +18,7 @@ const char secret[] =
     "dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg"
     "YnkK";
 
-struct entry *tab[HASHSIZ];
+struct entry *tab[CHAR_MAX];
 
 uint8_t *
 encrypt(uint8_t *in, size_t inlen, size_t *outlenp)
@@ -102,11 +101,11 @@ hash(uint8_t *blk, size_t blksiz)
 	for (h = 0; blksiz--;)
 		h = h * 31 + *blk++;
 
-	return h % HASHSIZ;
+	return h % CHAR_MAX;
 }
 
 char
-lookup(uint8_t *blk, size_t blksiz, char c, int creat)
+lookup(uint8_t *blk, size_t blksiz, char c, int create)
 {
 	uint8_t h;
 	struct entry *p;
@@ -117,7 +116,7 @@ lookup(uint8_t *blk, size_t blksiz, char c, int creat)
 		if (memcmp(blk, p->blk, blksiz) == 0)
 			goto done;
 
-	if (creat) {
+	if (create) {
 		if ((p = malloc(sizeof(*p))) == NULL ||
 		    (p->blk = malloc(blksiz)) == NULL)
 			goto fail;
@@ -126,7 +125,8 @@ lookup(uint8_t *blk, size_t blksiz, char c, int creat)
 		p->c = c;
 		p->next = tab[h];
 		tab[h] = p;
-	}
+	} else
+		goto fail;
 done:
 	return p->c;
 fail:
@@ -134,8 +134,90 @@ fail:
 }
 
 int
+fill_tab(size_t blksiz)
+{
+	uint8_t in[blksiz], *out;
+	char c;
+
+	memset(in, 'A', blksiz-1);
+
+	for (c = 0; c < CHAR_MAX; c++) {
+		in[blksiz-1] = c;
+		if ((out = encrypt(in, blksiz, NULL)) == NULL ||
+		    lookup(out, blksiz, c, 1) == -1)
+			goto fail;
+	}
+
+	return 1;
+fail:
+	return 0;
+}
+
+char *
+crack_secret(size_t blksiz)
+{
+	FILE *memstream;
+	char *in, tmp[BUFSIZ], *out, *blk, c;
+	size_t i, inlen, outlen;
+	BIO *b64_mem, *b64;
+	ssize_t nr;
+
+	if (fill_tab(blksiz) == 0 ||
+	    (memstream = open_memstream(&in, &inlen)) == NULL)
+		goto fail;
+
+	for (i = 0; i < blksiz-1; i++)
+		putc('A', memstream);
+
+	if ((b64_mem = BIO_new_mem_buf((char *) secret, strlen(secret))) == NULL ||
+	    (b64 = BIO_new(BIO_f_base64())) == NULL)
+		goto fail;
+
+	BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
+	BIO_push(b64, b64_mem);
+
+	while ((nr = BIO_read(b64, tmp, BUFSIZ)) > 0)
+		if (fwrite(tmp, nr, 1, memstream) < 1)
+			goto fail;
+	fclose(memstream);
+
+	BIO_free_all(b64);
+
+	if ((memstream = open_memstream(&out, &outlen)) == NULL)
+		goto fail;
+
+	while (inlen >= blksiz) {
+		if ((blk = encrypt(in, blksiz, NULL)) == NULL ||
+		    (c = lookup(blk, blksiz, 0, 0)) == -1)
+			goto fail;
+
+		putc(c, memstream);
+		memmove(in+blksiz-1, in+blksiz, inlen-blksiz);
+
+		free(blk);
+		inlen--;
+	}
+	fclose(memstream);
+
+	return out;
+fail:
+	return NULL;
+}
+
+int
 main(void)
 {
-	puts(is_ecb(crack_blksiz()) ? "yes" : "no");
-	return 0;
+	size_t blksiz;
+	char *buf;
+
+	blksiz = crack_blksiz();
+	if (!is_ecb(blksiz))
+		errx(1, "ECB required");
+
+	if ((buf = crack_secret(blksiz)) == NULL)
+		err(1, NULL);
+
+	puts(buf);
+
+	exit(0);
 }
