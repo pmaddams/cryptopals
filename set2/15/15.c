415b4a26deb7b0f1e11f9738fc2101b295d45335
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-01-11 23:16:48 -0500

    set2

--- set2/15/15.c
+++ set2/15/15.c
@@ -5,6 +5,46 @@
 
 #define BLKSIZ 16
 
+void
+proc(uint8_t *buf, size_t *lenp)
+{
+	size_t i, j;
+	char tmp[3];
+
+	for (i = j = 0; i < *lenp;)
+		switch (buf[i]) {
+		case '\\':
+			if (buf[i+1] == 'x') {
+				i += 2;
+				if (i < *lenp && isxdigit(buf[i])) {
+					memset(tmp, '\0', 3);
+					tmp[0] = buf[i++];
+					if (i < *lenp && isxdigit(buf[i]))
+						tmp[1] = buf[i++];
+					buf[j++] = strtol(tmp, NULL, 16);
+				}
+			} else if (buf[i+1] == '0') {
+				i += 2;
+				if (i < *lenp && isdigit(buf[i])) {
+					memset(tmp, '\0', 3);
+					tmp[0] = buf[i++];
+					if (i < *lenp && isdigit(buf[i]))
+						tmp[1] = buf[i++];
+					buf[j++] = strtol(tmp, NULL, 8);
+					tmp[0] = '\0';
+				}
+			}
+			break;
+		default:
+			buf[j++] = buf[i++];
+			break;
+		}
+
+	if (lenp != NULL)
+		*lenp = j;
+	buf[j] = '\0';
+}
+
 bool
 is_valid(uint8_t *buf, size_t len)
 {
@@ -42,7 +82,19 @@ done:
 int
 main(int argc, char **argv)
 {
-	char s[] = "ICE ICE BABY\x04\x04\x04\x04";
-	puts(is_valid(s, strlen(s)) ? "valid" : "invalid");
+	char *s;
+	size_t len;
+
+	while (argc > 1) {
+		s = argv[1];
+		len = strlen(s);
+		
+		proc(s, &len);
+		printf("%s: %s\n", s, is_valid(s, len) ? "valid" : "invalid");
+
+		argc--;
+		argv++;
+	}
+
 	return 0;
 }
