2e2f6b830b1587415cc0f8b6dcdff9d26cc9a4a0
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-01-13 00:22:09 -0500

    set2

--- set2/16/16.c
+++ set2/16/16.c
@@ -10,36 +10,27 @@
 uint8_t *
 encrypt(uint8_t *in, size_t inlen, size_t *outlenp)
 {
-	const uint8_t
+	const char
 	    *prefix = "comment1=cooking%20MCs;userdata=",
 	    *suffix = ";comment2=%20like%20a%20pound%20of%20bacon";
-	static size_t pfxlen, sfxlen;
 	static uint8_t key[BLKSIZ], iv[BLKSIZ];
-	static BIO *cip;
-	uint8_t *clean;
+	char *clean, *out, c, buf[BUFSIZ];
 	FILE *memstream;
-	char *out, c, buf[BUFSIZ];
-	size_t i, j, outlen;
+	size_t i, j, outlen, pfxlen, sfxlen;
+	BIO *cip, *bio_out;
 	ssize_t nr;
-
-	if (pfxlen == 0)
-		pfxlen = strlen(prefix);
-	if (sfxlen == 0)
-		sfxlen = strlen(suffix);
 
 	while (*key == '\0')
 		arc4random_buf(key, BLKSIZ);
 
-	if (cip == NULL) {
-		if ((cip = BIO_new(BIO_f_cipher())) == NULL)
-			goto fail;
-		BIO_set_cipher(cip, EVP_aes_128_cbc(), key, iv, 1);
-	} else
-		BIO_reset(cip);
-
 	if ((clean = malloc(inlen*3)) == NULL ||
-	    (memstream = open_memstream(&out, &outlen)) == NULL)
+	    (memstream = open_memstream(&out, &outlen)) == NULL ||
+	    (cip = BIO_new(BIO_f_cipher())) == NULL ||
+	    (bio_out = BIO_new_fp(memstream, BIO_NOCLOSE)) == NULL)
 		goto fail;
+
+	BIO_set_cipher(cip, EVP_aes_128_cbc(), key, iv, 1);
+	BIO_push(cip, bio_out);
 
 	for (i = j = 0; i < inlen; i++)
 		switch (c = in[i]) {
@@ -55,19 +46,18 @@ encrypt(uint8_t *in, size_t inlen, size_t *outlenp)
 		}
 
 	inlen = j;
+	pfxlen = strlen(prefix);
+	sfxlen = strlen(suffix);
 
 	if ((BIO_write(cip, prefix, pfxlen)) < pfxlen ||
 	    (BIO_write(cip, clean, inlen)) < inlen ||
 	    (BIO_write(cip, suffix, sfxlen)) < sfxlen)
 		goto fail;
+
 	BIO_flush(cip);
-
-	free(clean);
-
-	while ((nr = BIO_read(cip, buf, BUFSIZ)) > 0)
-		if (fwrite(buf, nr, 1, memstream) < 1)
-			goto fail;
 	fclose(memstream);
+	BIO_free_all(cip);
+	free(clean);
 
 	if (outlenp != NULL)
 		*outlenp = outlen;
@@ -80,10 +70,4 @@ fail:
 int
 main(void)
 {
-	uint8_t *enc;
-
-	if ((enc = encrypt("", 0, NULL)) == NULL)
-		err(1, NULL);
-
-	exit(0);
 }
