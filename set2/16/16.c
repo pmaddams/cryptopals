217274f908f8c222a36837b59ebe392c3937f473
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-01-13 00:47:27 -0500

    set2

--- set2/16/16.c
+++ set2/16/16.c
@@ -1,3 +1,5 @@
+#include <err.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,10 +7,16 @@
 #include <openssl/bio.h>
 #include <openssl/evp.h>
 
-#define BLKSIZ 16
+#define BLKSIZ	16
+
+#define MATCH	";admin=true;"
+#define CLOAK	29
+
+#define DECRYPT	0
+#define ENCRYPT	1
 
 uint8_t *
-encrypt(uint8_t *in, size_t inlen, size_t *outlenp)
+cbc_crypt(uint8_t *in, size_t inlen, size_t *outlenp, int enc)
 {
 	const char
 	    *prefix = "comment1=cooking%20MCs;userdata=",
@@ -29,7 +37,7 @@ encrypt(uint8_t *in, size_t inlen, size_t *outlenp)
 	    (bio_out = BIO_new_fp(memstream, BIO_NOCLOSE)) == NULL)
 		goto fail;
 
-	BIO_set_cipher(cip, EVP_aes_128_cbc(), key, iv, 1);
+	BIO_set_cipher(cip, EVP_aes_128_cbc(), key, iv, enc);
 	BIO_push(cip, bio_out);
 
 	for (i = j = 0; i < inlen; i++)
@@ -67,7 +75,38 @@ fail:
 	return NULL;
 }
 
+bool
+is_admin(uint8_t *buf, size_t len)
+{
+	return memmem(buf, len, MATCH, strlen(MATCH)) ? true : false;
+}
+
 int
 main(void)
 {
+	uint8_t *in, *enc, *out;
+	size_t i, matchlen, inlen, enclen, outlen;
+
+	matchlen = strlen(MATCH);
+	inlen = BLKSIZ + matchlen;
+
+	if ((in = malloc(inlen)) == NULL)
+		err(1, NULL);
+
+	memset(in, 'A', BLKSIZ);
+	for (i = 0; i < matchlen; i++)
+		in[BLKSIZ+i] = MATCH[i] ^ CLOAK;
+
+	if ((enc = cbc_crypt(in, inlen, &enclen, ENCRYPT)) == NULL)
+		err(1, NULL);
+
+	for (i = 0; i < matchlen; i++)
+		enc[BLKSIZ*2+i] ^= CLOAK;
+
+	if ((out = cbc_crypt(enc, enclen, &outlen, DECRYPT)) == NULL)
+		err(1, NULL);
+
+	printf("admin=%s\n", is_admin(out, outlen) ? "true" : "false");
+
+	exit(0);
 }
