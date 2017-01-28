7f73496e383f3a0e69e965293a1ab0218cba3330
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-01-27 21:34:25 -0500

    set5

--- set5/36/36-client.c
+++ set5/36/36-client.c
@@ -5,11 +5,18 @@
 
 #include <err.h>
 #include <netdb.h>
+#include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "36.h"
+
+#define POLL_STDIN	0
+#define POLL_NETOUT	1
+#define POLL_NETIN	2
+#define POLL_STDOUT	3
 
 int
 lo_connect(in_port_t port)
@@ -34,10 +41,39 @@ fail:
 int
 main(void)
 {
-	int fd, c;
+	int fd;
+	struct pollfd pfd[4];
+	char buf[BUFSIZ];
+	ssize_t nr;
 
 	if ((fd = lo_connect(PORT)) == -1)
 		err(1, NULL);
 
-	exit(0);
+	pfd[POLL_STDIN].fd = STDIN_FILENO;
+	pfd[POLL_STDIN].events = POLLIN;
+
+	pfd[POLL_NETOUT].fd = fd;
+	pfd[POLL_NETOUT].events = 0;
+
+	pfd[POLL_NETIN].fd = fd;
+	pfd[POLL_NETIN].events = POLLIN;
+
+	pfd[POLL_STDOUT].fd = STDOUT_FILENO;
+	pfd[POLL_STDOUT].events = 0;
+
+	for (;;) {
+		if (poll(pfd, 4, INFTIM) == -1)
+			err(1, NULL);
+
+		if (pfd[POLL_STDIN].revents & POLLIN) {
+			if ((nr = read(STDIN_FILENO, buf, BUFSIZ)) == -1 ||
+			    write(fd, buf, nr) < nr)
+				err(1, NULL);
+		}
+		if (pfd[POLL_NETIN].revents & POLLIN) {
+			if ((nr = read(fd, buf, BUFSIZ)) == -1 ||
+			    write(STDOUT_FILENO, buf, nr) < nr)
+				err(1, NULL);
+		}
+	}
 }
