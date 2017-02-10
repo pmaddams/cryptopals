5992cb6ffd066a75e163d30258e731209c73d77b
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-02-09 21:58:49 -0500

    set5

--- set5/38/38-server.c
+++ set5/38/38-server.c
@@ -169,7 +169,7 @@ main(void)
 {
 	int listenfd, connfd;
 	pid_t pid;
-	char *buf, *buf2, *p;
+	char *buf, *buf1, *p;
 	size_t i;
 
 	if (init_params(&modulus, &generator) == 0 ||
@@ -206,12 +206,12 @@ main(void)
 		free(buf);
 
 		if ((buf = BN_bn2hex(public_key)) == NULL ||
-		    (buf2 = BN_bn2hex(scrambler)) == NULL ||
-		    ssendf(connfd, "%s %s %s", salt, buf, buf2) == 0)
+		    (buf1 = BN_bn2hex(scrambler)) == NULL ||
+		    ssendf(connfd, "%s %s %s", salt, buf, buf1) == 0)
 			err(1, NULL);
 
 		free(buf);
-		free(buf2);
+		free(buf1);
 
 		if ((client_hmac = srecv(connfd)) == NULL)
 			err(1, NULL);
