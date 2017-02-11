6779ef144f57432faf8468d3610e2c6c23928783
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-02-10 19:28:21 -0500

    set5

--- set5/39/.bak/39.c
+++ set5/39/.bak/39.c
@@ -1,19 +1,4 @@
-#include <err.h>
-#include <string.h>
-
 #include <openssl/bn.h>
-
-#define E	"3"
-
-#define BITS	2048
-
-struct rsa {
-	BIGNUM *p;
-	BIGNUM *q;
-	BIGNUM *n;
-	BIGNUM *e;
-	BIGNUM *d;
-};
 
 BIGNUM *
 invmod(BIGNUM *n, BIGNUM *modulus)
@@ -70,55 +55,4 @@ done:
 	return res;
 fail:
 	return NULL;
-}
-
-int
-rsa_init(struct rsa *rsa)
-{
-	BN_CTX *ctx;
-	BIGNUM *totient, *t0, *t1;
-
-	if ((ctx = BN_CTX_new()) == NULL)
-		goto fail;
-	BN_CTX_start(ctx);
-
-	memset(rsa, 0, sizeof(*rsa));
-
-	if ((rsa->p = BN_new()) == NULL ||
-	    (rsa->q = BN_new()) == NULL ||
-	    (rsa->n = BN_new()) == NULL ||
-
-	    (totient = BN_CTX_get(ctx)) == NULL ||
-	    (t0 = BN_CTX_get(ctx)) == NULL ||
-	    (t1 = BN_CTX_get(ctx)) == NULL ||
-
-	    BN_generate_prime_ex(rsa->p, BITS, 0, NULL, NULL, NULL) == 0 ||
-	    BN_generate_prime_ex(rsa->q, BITS, 0, NULL, NULL, NULL) == 0 ||
-	    BN_mul(rsa->n, rsa->p, rsa->q, ctx) == 0 ||
-	    BN_dec2bn(&rsa->e, E) == 0 ||
-
-	    BN_sub(t0, rsa->p, BN_value_one()) == 0 ||
-	    BN_sub(t1, rsa->q, BN_value_one()) == 0 ||
-	    BN_mul(totient, t0, t1, ctx) == 0 ||
-
-	    (rsa->d = invmod(rsa->e, totient)) == NULL)
-		goto fail;
-
-	BN_CTX_end(ctx);
-	BN_CTX_free(ctx);
-
-	return 1;
-fail:
-	return 0;
-}
-
-int
-main(void)
-{
-	struct rsa rsa;
-
-	if (rsa_init(&rsa) == 0)
-		err(1, NULL);
-
-	return 0;
 }
