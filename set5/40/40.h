5f747732b935a5f9be78e6c372e8dbc6a02ebb9d
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-02-15 21:56:18 -0500

    set5

--- set5/40/40.h
+++ set5/40/40.h
@@ -1,16 +1,7 @@
 #include <openssl/bn.h>
 
-#define VERBOSE
-
 #define E	"3"
 #define BITS	2048
-
-struct rsa {
-	BIGNUM *p;
-	BIGNUM *q;
-	BIGNUM *n;
-	BIGNUM *e;
-};
 
 int cubert(BIGNUM *, BIGNUM *, BN_CTX *);
 int invmod(BIGNUM *, BIGNUM *, BIGNUM *, BN_CTX *);
--- set5/40/main.c
+++ set5/40/main.c
@@ -4,168 +4,119 @@
 #include <string.h>
 
 #include <openssl/bn.h>
+#include <openssl/rsa.h>
 
 #include "40.h"
 
 int
-rsa_init(struct rsa *rsa)
+crack_rsa(BIGNUM *res, BIGNUM *c1, BIGNUM *n1, BIGNUM *c2, BIGNUM *n2, BIGNUM *c3, BIGNUM *n3)
 {
 	BN_CTX *ctx;
-
-#ifdef VERBOSE
-	fprintf(stderr, "initializing, please wait...");
-#endif
-
-	if ((ctx = BN_CTX_new()) == NULL)
-		goto fail;
-
-	memset(rsa, 0, sizeof(*rsa));
-
-	if ((rsa->p = BN_new()) == NULL ||
-	    (rsa->q = BN_new()) == NULL ||
-	    (rsa->n = BN_new()) == NULL ||
-
-	    BN_generate_prime_ex(rsa->p, BITS, 0, NULL, NULL, NULL) == 0 ||
-	    BN_generate_prime_ex(rsa->q, BITS, 0, NULL, NULL, NULL) == 0 ||
-
-	    BN_mul(rsa->n, rsa->p, rsa->q, ctx) == 0 ||
-
-	    BN_dec2bn(&rsa->e, E) == 0)
-		goto fail;
-
-	BN_CTX_free(ctx);
-
-#ifdef VERBOSE
-	fprintf(stderr, "done.\n");
-#endif
-
-	return 1;
-fail:
-	return 0;
-}
-
-BIGNUM *
-rsa_encrypt(struct rsa *rsa, uint8_t *buf, size_t len)
-{
-	BN_CTX *ctx;
-	BIGNUM *in, *out;
+	BIGNUM *tmp, *out;
 
 	if ((ctx = BN_CTX_new()) == NULL)
 		goto fail;
 	BN_CTX_start(ctx);
 
-	if ((in = BN_CTX_get(ctx)) == NULL ||
-	    (out = BN_new()) == NULL ||
-	    BN_bin2bn(buf, len, in) == 0 ||
-	    BN_mod_exp(out, in, rsa->e, rsa->n, ctx) == 0)
-		goto fail;
+	if ((tmp = BN_CTX_get(ctx)) == NULL ||
+	    (out = BN_CTX_get(ctx)) == NULL ||
 
-	BN_CTX_end(ctx);
-	BN_CTX_free(ctx);
-
-	return out;
-fail:
-	return NULL;
-}
-
-BIGNUM *
-crack_rsa(BIGNUM *c1, BIGNUM *n1, BIGNUM *c2, BIGNUM *n2, BIGNUM *c3, BIGNUM *n3)
-{
-	BN_CTX *ctx;
-	BIGNUM *res, *tmp;
-
-	if ((ctx = BN_CTX_new()) == NULL)
-		goto fail;
-	BN_CTX_start(ctx);
-
-	if ((res = BN_new()) == NULL ||
-	    (tmp = BN_CTX_get(ctx)) == NULL ||
-
-	    BN_zero(res) == 0 ||
+	    BN_zero(out) == 0 ||
 
 	    BN_mul(tmp, n2, n3, ctx) == 0 ||
 	    invmod(tmp, tmp, n1, ctx) == 0 ||
 	    BN_mul(tmp, tmp, c1, ctx) == 0 ||
 	    BN_mul(tmp, tmp, n2, ctx) == 0 ||
 	    BN_mul(tmp, tmp, n3, ctx) == 0 ||
-	    BN_add(res, res, tmp) == 0 ||
+	    BN_add(out, out, tmp) == 0 ||
 
 	    BN_mul(tmp, n1, n3, ctx) == 0 ||
 	    invmod(tmp, tmp, n2, ctx) == 0 ||
 	    BN_mul(tmp, tmp, c2, ctx) == 0 ||
 	    BN_mul(tmp, tmp, n1, ctx) == 0 ||
 	    BN_mul(tmp, tmp, n3, ctx) == 0 ||
-	    BN_add(res, res, tmp) == 0 ||
+	    BN_add(out, out, tmp) == 0 ||
 
 	    BN_mul(tmp, n1, n2, ctx) == 0 ||
 	    invmod(tmp, tmp, n3, ctx) == 0 ||
 	    BN_mul(tmp, tmp, c3, ctx) == 0 ||
 	    BN_mul(tmp, tmp, n1, ctx) == 0 ||
 	    BN_mul(tmp, tmp, n2, ctx) == 0 ||
-	    BN_add(res, res, tmp) == 0 ||
+	    BN_add(out, out, tmp) == 0 ||
 
 	    BN_mul(tmp, n1, n2, ctx) == 0 ||
 	    BN_mul(tmp, tmp, n3, ctx) == 0 ||
 
-	    BN_mod(res, res, tmp, ctx) == 0 ||
+	    BN_mod(out, out, tmp, ctx) == 0 ||
 
-	    cubert(res, res, ctx) == 0)
+	    cubert(out, out, ctx) == 0 ||
+
+	    BN_copy(res, out) == NULL)
 		goto fail;
 
 	BN_CTX_end(ctx);
 	BN_CTX_free(ctx);
 
-	return res;
+	return 1;
 fail:
-	return NULL;
+	return 0;
 }
 
 int
 main(int argc, char **argv)
 {
-	struct rsa r1, r2, r3;
-	char *in, *out;
-	size_t inlen, outlen;
-	BIGNUM *c1, *c2, *c3, *p;
+	RSA *r1, *r2, *r3;
+	BIGNUM *e, *c1, *c2, *c3, *p;
+	ssize_t rsa_size;
+	char *s, *enc, *dec;
 
 	if (argc == 1) {
 		fprintf(stderr, "usage: %s string ...\n", argv[0]);
 		exit(1);
 	}
 
-	if (rsa_init(&r1) == 0 ||
-	    rsa_init(&r2) == 0 ||
-	    rsa_init(&r3) == 0 ||
+	if ((r1 = RSA_new()) == NULL ||
+	    (r2 = RSA_new()) == NULL ||
+	    (r3 = RSA_new()) == NULL ||
 
+	    (e = BN_new()) == NULL ||
 	    (c1 = BN_new()) == NULL ||
 	    (c2 = BN_new()) == NULL ||
-	    (c3 = BN_new()) == NULL)
+	    (c3 = BN_new()) == NULL ||
+	    (p = BN_new()) == NULL ||
+
+	    BN_dec2bn(&e, E) == 0 ||
+
+	    RSA_generate_key_ex(r1, BITS, e, NULL) == 0 ||
+	    RSA_generate_key_ex(r2, BITS, e, NULL) == 0 ||
+	    RSA_generate_key_ex(r3, BITS, e, NULL) == 0)
+		err(1, NULL);
+
+	BN_free(e);
+
+	rsa_size = RSA_size(r1);
+	if ((enc = malloc(rsa_size)) == NULL ||
+	    (dec = malloc(rsa_size)) == NULL)
 		err(1, NULL);
 
 	while (argc > 1) {
-		in = argv[1];
-		inlen = strlen(in);
+		s = argv[1];
 
-		if ((c1 = rsa_encrypt(&r1, in, inlen)) == NULL ||
-		    (c2 = rsa_encrypt(&r2, in, inlen)) == NULL ||
-		    (c3 = rsa_encrypt(&r3, in, inlen)) == NULL ||
+		if (RSA_public_encrypt(rsa_size, s, enc, r1, RSA_NO_PADDING) == 0 ||
+		    BN_bin2bn(enc, rsa_size, c1) == NULL ||
 
-		    (p = crack_rsa(c1, r1.n, c2, r2.n, c3, r3.n)) == NULL)
+		    RSA_public_encrypt(rsa_size, s, enc, r2, RSA_NO_PADDING) == 0 ||
+		    BN_bin2bn(enc, rsa_size, c2) == NULL ||
+
+		    RSA_public_encrypt(rsa_size, s, enc, r3, RSA_NO_PADDING) == 0 ||
+		    BN_bin2bn(enc, rsa_size, c3) == NULL ||
+
+		    crack_rsa(p, c1, r1->n, c2, r2->n, c3, r3->n) == 0 ||
+
+		    BN_bn2bin(p, dec) == 0)
 			err(1, NULL);
 
-		outlen = BN_num_bytes(p);
-		if ((out = malloc(outlen+1)) == NULL ||
-		    BN_bn2bin(p, out) == 0)
-			err(1, NULL);
-		out[outlen] = '\0';
-
-		puts(out);
-
-		free(c1);
-		free(c2);
-		free(c3);
-		free(p);
-		free(out);
+		puts(dec);
 
 		argc--;
 		argv++;
