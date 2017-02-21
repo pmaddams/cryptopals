90f83fc571fe30af1686a33567a8d97dc402974e
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-02-21 16:14:22 -0500

    set6

---
+++ set6/42/.bak/main.c
@@ -0,0 +1,52 @@
+#include <sys/types.h>
+
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <openssl/bn.h>
+#include <openssl/rsa.h>
+
+#include "42.h"
+
+char *
+rsa_sign(RSA *rsa, uint8_t *buf, size_t len)
+{
+}
+
+char *
+rsa_forge(RSA *rsa, uint8_t *buf, size_t len)
+{
+}
+
+int
+rsa_verify_strong(RSA *rsa, char *sig)
+{
+}
+
+int
+rsa_verify_weak(RSA *rsa, char *sig)
+{
+}
+
+int
+main(void)
+{
+	RSA *rsa;
+	BIGNUM *e;
+	uint8_t *asn1;
+	size_t len;
+
+	if ((rsa = RSA_new()) == NULL ||
+	    (e = BN_new()) == NULL ||
+
+	    BN_dec2bn(&e, E) == 0 ||
+
+	    RSA_generate_key_ex(rsa, BITS, e, NULL) == 0 ||
+
+	    (asn1 = make_asn1(DATA, strlen(DATA), &len)) == NULL)
+		err(1, NULL);
+
+	exit(0);
+}
--- set6/42/main.c
+++ set6/42/main.c
@@ -10,24 +10,68 @@
 
 #include "42.h"
 
-char *
+uint8_t *
 rsa_sign(RSA *rsa, uint8_t *buf, size_t len)
 {
+	uint8_t *tmp, *res, *asn;
+	size_t rsa_size, asnlen;
+
+	rsa_size = RSA_size(rsa);
+	if ((tmp = malloc(rsa_size)) == NULL ||
+	    (res = malloc(rsa_size)) == NULL ||
+
+	    (asn = make_asn1(buf, len, &asnlen)) == NULL ||
+
+	    RSA_padding_add_PKCS1_type_1(tmp, rsa_size, asn, asnlen) == 0 ||
+	    RSA_private_encrypt(rsa_size, tmp, res, rsa, RSA_PKCS1_PADDING) == 0)
+		goto fail;
+
+	free(tmp);
+	free(asn);
+
+	return res;
+fail:
+	return NULL;
 }
 
-char *
+uint8_t *
 rsa_forge(RSA *rsa, uint8_t *buf, size_t len)
 {
 }
 
 int
-rsa_verify_strong(RSA *rsa, char *sig)
+rsa_verify_strong(RSA *rsa, uint8_t *buf, size_t len, uint8_t *sig)
 {
 }
 
 int
-rsa_verify_weak(RSA *rsa, char *sig)
+rsa_verify_weak(RSA *rsa, uint8_t *buf, size_t len, uint8_t *sig)
 {
+	uint8_t *dec, *asn, *p1, *p2;
+	size_t rsa_size, asnlen;
+
+	rsa_size = RSA_size(rsa);
+	if ((dec = malloc(rsa_size)) == NULL ||
+
+	    RSA_public_decrypt(rsa_size, sig, dec, rsa, RSA_PKCS1_PADDING) == 0 ||
+
+	    (asn = make_asn1(buf, len, &asnlen)) == NULL)
+		goto fail;
+
+	for (p1 = &dec[0]; *p1++ != '\x00';)
+		continue;
+	for (p2 = &asn[1]; *p2++ != '\x00';)
+		continue;
+
+	if (memcmp(p1, p2, asnlen) != 0)
+		goto fail;
+
+	free(dec);
+	free(asn);
+
+	return 1;
+fail:
+	return 0;
 }
 
 int
@@ -35,17 +79,13 @@ main(void)
 {
 	RSA *rsa;
 	BIGNUM *e;
-	uint8_t *asn1;
-	size_t len;
 
 	if ((rsa = RSA_new()) == NULL ||
 	    (e = BN_new()) == NULL ||
 
 	    BN_dec2bn(&e, E) == 0 ||
 
-	    RSA_generate_key_ex(rsa, BITS, e, NULL) == 0 ||
-
-	    (asn1 = make_asn1(DATA, strlen(DATA), &len)) == NULL)
+	    RSA_generate_key_ex(rsa, BITS, e, NULL) == 0)
 		err(1, NULL);
 
 	exit(0);
