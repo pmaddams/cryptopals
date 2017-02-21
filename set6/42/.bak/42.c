eeaaa55117264dc88ca0cd2987e19cb0cd834464
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-02-20 20:21:11 -0500

    set6

--- set6/42/.bak/42.c
+++ set6/42/.bak/42.c
@@ -2,6 +2,7 @@
 
 #include <err.h>
 #include <sha2.h>
+#include <string.h>
 
 #include <openssl/asn1.h>
 #include <openssl/bn.h>
@@ -15,12 +16,40 @@
 #define BITS	1024
 
 char *
-rsa_sign(RSA *rsa, char *buf)
+rsa_sign(RSA *rsa, char *buf, size_t len)
 {
+	X509_SIG sig;
+	X509_ALGOR algor;
+	ASN1_TYPE parameter;
+	ASN1_OCTET_STRING digest;
+	ssize_t rsa_size, siglen;
+
+	sig.algor = &algor;
+	if ((sig.algor->algorithm = OBJ_nid2obj(NID_sha256)) == NULL)
+		goto fail;
+
+	parameter.type = V_ASN1_NULL;
+	parameter.value.ptr = NULL;
+	sig.algor->parameter = &parameter;
+
+	sig.digest = &digest;
+	sig.digest->data = buf;
+	sig.digest->length = len;
+
+	rsa_size = RSA_size(rsa);
+	if ((siglen = i2d_X509_SIG(&sig, NULL)) <= 0 ||
+	    siglen + RSA_PKCS1_PADDING_SIZE > rsa_size)
+		goto fail;
+
+	warnx("signature length %d", siglen);
+	warnx("padding length %d", RSA_PKCS1_PADDING_SIZE);
+	warnx("rsa size %d", RSA_size(rsa));
+fail:
+	return NULL;
 }
 
 char *
-rsa_forge(RSA *rsa, char *buf)
+rsa_forge(RSA *rsa, char *buf, size_t len)
 {
 }
 
@@ -42,4 +71,8 @@ main(void)
 
 	    RSA_generate_key_ex(rsa, BITS, e, NULL) == 0)
 		err(1, NULL);
+
+	rsa_sign(rsa, DATA, strlen(DATA));
+
+	return 0;
 }
