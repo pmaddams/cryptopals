6cfa2e2d08f6edc1e621ec70473964d39e64064a
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-02-20 19:56:45 -0500

    set6

--- set6/42/.bak/42.c
+++ set6/42/.bak/42.c
@@ -1,14 +1,45 @@
+#include <sys/types.h>
+
+#include <err.h>
+#include <sha2.h>
+
 #include <openssl/asn1.h>
+#include <openssl/bn.h>
 #include <openssl/objects.h>
 #include <openssl/rsa.h>
 #include <openssl/x509.h>
 
-#define DATA "hi mom"
+#define DATA	"hi mom"
 
-rsa_sign()
+#define E	"3"
+#define BITS	1024
+
+char *
+rsa_sign(RSA *rsa, char *buf)
 {
 }
 
-main()
+char *
+rsa_forge(RSA *rsa, char *buf)
 {
+}
+
+int
+rsa_verify(RSA *rsa, char *sig)
+{
+}
+
+int
+main(void)
+{
+	RSA *rsa;
+	BIGNUM *e;
+
+	if ((rsa = RSA_new()) == NULL ||
+	    (e = BN_new()) == NULL ||
+
+	    BN_dec2bn(&e, E) == 0 ||
+
+	    RSA_generate_key_ex(rsa, BITS, e, NULL) == 0)
+		err(1, NULL);
 }
