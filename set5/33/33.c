10a46b6e5fa349e90f6ffa9efad9db0ef1611580
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-02-23 22:58:27 -0500

    set5

--- set5/33/33.c
+++ set5/33/33.c
@@ -1,4 +1,7 @@
+#include <sys/types.h>
+
 #include <err.h>
+#include <sha2.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,29 +18,44 @@
 		"fffffffffffff"
 #define G	"2"
 
-struct party {
-	BIGNUM private;
-	BIGNUM public;
-	BIGNUM shared;
+#define BITS	2048
+
+#define KEYSIZE	16
+
+struct dh {
+	BIGNUM *p;
+	BIGNUM *g;
+	BIGNUM *pub_key;
+	BIGNUM *priv_key;
 };
 
-BIGNUM *p, *g;
+struct party {
+	struct dh *dh;
+	uint8_t enc_key[KEYSIZE];
+	uint8_t mac_key[KEYSIZE];
+};
 
 int
-dh_params(struct party *party)
+dh_init(struct party *party)
 {
 	BN_CTX *ctx;
-	uint8_t buf[BUFSIZ];
+	struct dh *dh;
 
-	if ((ctx = BN_CTX_new()) == NULL)
+	if ((ctx = BN_CTX_new()) == NULL ||
+
+	    (dh = malloc(sizeof(*dh))) == NULL ||
+	    (dh->p = BN_new()) == NULL ||
+	    (dh->g = BN_new()) == NULL ||
+	    (dh->pub_key = BN_new()) == NULL ||
+	    (dh->priv_key = BN_new()) == NULL ||
+
+	    BN_hex2bn(&dh->p, P) == 0 ||
+	    BN_hex2bn(&dh->g, G) == 0 ||
+	    BN_rand(dh->priv_key, BITS, 0, 0) == 0 ||
+	    BN_mod_exp(dh->pub_key, dh->g, dh->priv_key, dh->p, ctx) == 0)
 		goto fail;
 
-	memset(party, 0, sizeof(*party));
-	arc4random_buf(buf, BUFSIZ);
-
-	if (BN_bin2bn(buf, BUFSIZ, &party->private) == 0 ||
-	    BN_mod_exp(&party->public, g, &party->private, p, ctx) == 0)
-		goto fail;
+	party->dh = dh;
 
 	BN_CTX_free(ctx);
 	return 1;
@@ -48,23 +66,58 @@ fail:
 int
 dh_exchange(struct party *p1, struct party *p2)
 {
-	BN_CTX *ctx;
+	BN_CTX *bnctx;
+	BIGNUM *secret;
+	size_t len;
+	uint8_t *buf, hash[SHA256_DIGEST_LENGTH];
+	SHA2_CTX sha2ctx;
 
-	if ((ctx = BN_CTX_new()) == NULL ||
-	    BN_mod_exp(&p1->shared, &p2->public, &p1->private, p, ctx) == 0 ||
-	    BN_mod_exp(&p2->shared, &p1->public, &p2->private, p, ctx) == 0)
+	if ((bnctx = BN_CTX_new()) == NULL)
+		goto fail;
+	BN_CTX_start(bnctx);
+
+	if ((secret = BN_CTX_get(bnctx)) == NULL ||
+	    BN_mod_exp(secret, p2->dh->pub_key, p1->dh->priv_key, p1->dh->p, bnctx) == 0)
 		goto fail;
 
-	BN_CTX_free(ctx);
+	len = BN_num_bytes(secret);
+	if ((buf = malloc(len)) == NULL ||
+	    BN_bn2bin(secret, buf) == 0)
+		goto fail;
+
+	SHA256Init(&sha2ctx);
+	SHA256Update(&sha2ctx, buf, len);
+	SHA256Final(hash, &sha2ctx);
+
+	memcpy(p1->enc_key, hash, KEYSIZE);
+	memcpy(p1->mac_key, hash+KEYSIZE, KEYSIZE);
+
+	BN_CTX_end(bnctx);
+	BN_CTX_free(bnctx);
+
 	return 1;
 fail:
 	return 0;
 }
 
 int
-dh_verify(struct party *p1, struct party *p2)
+dh_verify(struct party *p1, struct party *p2, char *message)
 {
-	return BN_cmp(&p1->shared, &p2->shared) == 0;
+	SHA2_CTX ctx;
+	uint8_t mac1[SHA256_DIGEST_LENGTH],
+	    mac2[SHA256_DIGEST_LENGTH];
+
+	SHA256Init(&ctx);
+	SHA256Update(&ctx, p1->mac_key, KEYSIZE);
+	SHA256Update(&ctx, message, strlen(message));
+	SHA256Final(mac1, &ctx);
+
+	SHA256Init(&ctx);
+	SHA256Update(&ctx, p2->mac_key, KEYSIZE);
+	SHA256Update(&ctx, message, strlen(message));
+	SHA256Final(mac2, &ctx);
+
+	return memcmp(mac1, mac2, SHA256_DIGEST_LENGTH) == 0;
 }
 
 int
@@ -72,16 +125,14 @@ main(void)
 {
 	struct party alice, bob;
 
-	if (BN_hex2bn(&p, P) == 0 ||
-	    BN_hex2bn(&g, G) == 0 ||
+	if (dh_init(&alice) == 0 ||
+	    dh_init(&bob) == 0 ||
 
-	    dh_params(&alice) == 0 ||
-	    dh_params(&bob) == 0 ||
-
-	    dh_exchange(&alice, &bob) == 0)
+	    dh_exchange(&alice, &bob) == 0 ||
+	    dh_exchange(&bob, &alice) == 0)
 		err(1, NULL);
 
-	puts(dh_verify(&alice, &bob) ? "success" : "failure");
+	puts(dh_verify(&alice, &bob, "OK") ? "success" : "failure");
 
 	exit(0);
 }
