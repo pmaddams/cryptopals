243314a3fb1585074bdcf71aade2d4b25e335841
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-02-24 23:05:50 -0500

    set1

--- set1/5/5.c
+++ set1/5/5.c
@@ -9,11 +9,9 @@ main(void)
 	size_t i, len;
 	int c;
 
-	i = 0;
 	len = strlen(KEY);
-
-	while ((c = getchar()) != EOF)
-		printf("%02x", c ^ KEY[i++ % len]);
+	for (i = 0; (c = getchar()) != EOF; i++)
+		printf("%02x", c ^ KEY[i % len]);
 	putchar('\n');
 
 	return 0;
