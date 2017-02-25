a0a89278b1d8fcb5f09fae0206ca714e403b33ee
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-02-24 21:11:19 -0500

    set1

--- set1/5/5.c
+++ set1/5/5.c
@@ -4,26 +4,16 @@
 #define KEY "ICE"
 
 int
-encrypt(int c)
-{
-	static size_t i, len;
-
-	if (len == 0)
-		len = strlen(KEY);
-
-	if (c != EOF)
-		c ^= KEY[i++ % len];
-
-	return c;
-}
-
-int
 main(void)
 {
+	size_t i, len;
 	int c;
 
-	while ((c = encrypt(getchar())) != EOF)
-		printf("%02x", c);
+	i = 0;
+	len = strlen(KEY);
+
+	while ((c = getchar()) != EOF)
+		printf("%02x", c ^ KEY[i++ % len]);
 	putchar('\n');
 
 	return 0;
