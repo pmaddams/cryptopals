13ba89849ee62fd21cce093c9ff8b57c36678106
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-02-27 22:19:22 -0500

    set5

--- set5/37/37-server.c
+++ set5/37/37-server.c
@@ -51,13 +51,14 @@ done:
 	return res;
 }
 
-char *
-generate_salt(void)
+int
+generate_salt(struct state *server)
 {
 	uint32_t num;
 
 	num = arc4random();
-	return atox((uint8_t *) &num, sizeof(num));
+
+	return (server->salt = atox((uint8_t *) &num, sizeof(num))) != NULL;
 }
 
 int
@@ -143,8 +144,7 @@ server_init(struct state *server)
 		goto fail;
 	arc4random_buf(server->password, KEYSIZE);
 
-	if ((server->salt = generate_salt()) == NULL ||
-
+	if (generate_salt(server) == 0 ||
 	    generate_verifier(server) == 0 ||
 
 	    srp_generate_server_priv_key(server->srp) == 0 ||
@@ -156,9 +156,47 @@ fail:
 	return 0;
 }
 
-/*
-BIGNUM *
-make_scrambler(BIGNUM *client_pubkey, BIGNUM *server_pubkey)
+int
+get_username_and_client_pub_key(int connfd, struct state *server, BIGNUM **bp)
+{
+	char *buf, *p;
+	size_t i;
+
+	if ((p = buf = srecv(connfd)) == NULL ||
+	    (i = strcspn(p, " ")) > strlen(p)-2)
+		goto fail;
+
+	p[i] = '\0';
+	if (strcmp(p, server->username) != 0)
+		goto fail;
+
+	p += i+1;
+	if (BN_hex2bn(bp, p) == 0)
+		goto fail;
+
+	free(buf);
+
+	return 1;
+fail:
+	return 0;
+}
+
+int
+send_salt_and_server_pub_key(int connfd, struct state *server)
+{
+	char *buf;
+
+	if ((buf = BN_bn2hex(server->srp->pub_key)) == NULL ||
+	    ssendf(connfd, "%s %s", server->salt, buf) == 0)
+
+	free(buf);
+	return 1;
+fail:
+	return 0;
+}
+
+int
+generate_scrambler(struct state *server, BIGNUM *client_pub_key)
 {
 	SHA2_CTX sha2ctx;
 	size_t len;
@@ -166,18 +204,17 @@ make_scrambler(BIGNUM *client_pubkey, BIGNUM *server_pubkey)
 
 	SHA256Init(&sha2ctx);
 
-	len = BN_num_bytes(client_pubkey);
-	if ((buf = malloc(len+1)) == NULL)
+	len = BN_num_bytes(client_pub_key);
+	if ((buf = malloc(len)) == NULL ||
+	    BN_bn2bin(client_pub_key, buf) == 0)
 		goto fail;
-
-	BN_bn2bin(client_pubkey, buf);
 
 	SHA256Update(&sha2ctx, buf, len);
 	free(buf);
 
-	len = BN_num_bytes(server_pubkey);
+	len = BN_num_bytes(server->srp->pub_key);
 	if ((buf = malloc(len)) == NULL ||
-	    BN_bn2bin(server_pubkey, buf) == 0)
+	    BN_bn2bin(server->srp->pub_key, buf) == 0)
 		goto fail;
 
 	SHA256Update(&sha2ctx, buf, len);
@@ -185,72 +222,83 @@ make_scrambler(BIGNUM *client_pubkey, BIGNUM *server_pubkey)
 
 	SHA256Final(hash, &sha2ctx);
 
-	return BN_bin2bn(hash, SHA256_DIGEST_LENGTH, NULL);
+	return BN_bin2bn(hash, SHA256_DIGEST_LENGTH, server->srp->u) != NULL;
 fail:
-	return NULL;
+	return 0;
 }
 
-BIGNUM *
-make_shared_s(BIGNUM *client_pubkey, BIGNUM *verifier, BIGNUM *scrambler, BIGNUM *private_key, BIGNUM *modulus)
+int
+server_generate_enc_key(struct state *server, BIGNUM *client_pub_key)
 {
 	BN_CTX *bnctx;
-	BIGNUM *tmp;
+	BIGNUM *secret;
+	size_t len;
+	uint8_t *buf,
+	    hash[SHA256_DIGEST_LENGTH];
+	SHA2_CTX sha2ctx;
 
-	if ((bnctx = BN_CTX_new()) == NULL)
+	if (generate_scrambler(server, client_pub_key) == 0 ||
+
+	    (bnctx = BN_CTX_new()) == NULL)
 		goto fail;
 	BN_CTX_start(bnctx);
 
-	if ((shared_s = BN_new()) == NULL ||
-	    (tmp = BN_CTX_get(bnctx)) == NULL ||
+	if ((secret = BN_CTX_get(bnctx)) == NULL ||
 
-	    BN_mod_exp(tmp, verifier, scrambler, modulus, bnctx) == 0 ||
-	    BN_mul(tmp, client_pubkey, tmp, bnctx) == 0 ||
-	    BN_mod_exp(shared_s, tmp, private_key, modulus, bnctx) == 0)
+	    BN_mod_exp(secret, server->srp->v, server->srp->u, server->srp->n, bnctx) == 0 ||
+	    BN_mul(secret, client_pub_key, secret, bnctx) == 0 ||
+	    BN_mod_exp(secret, secret, server->srp->priv_key, server->srp->n, bnctx) == 0)
 		goto fail;
+
+	len = BN_num_bytes(secret);
+	if ((buf = malloc(len)) == NULL ||
+
+	    BN_bn2bin(secret, buf) == 0)
+		goto fail;
+
+	SHA256Init(&sha2ctx);
+	SHA256Update(&sha2ctx, buf, len);
+	SHA256Final(hash, &sha2ctx);
+
+	memcpy(server->enc_key, hash, KEYSIZE);
 
 	BN_CTX_end(bnctx);
 	BN_CTX_free(bnctx);
+	free(buf);
 
-	return shared_s;
+	return 1;
 fail:
-	return NULL;
+	return 0;
 }
 
-char *
-make_shared_k(BIGNUM *shared_s)
+int
+server_verify_hmac(int connfd, struct state *server)
 {
-	size_t len;
-	char *buf, *res;
+	char *buf, hmac[SHA256_DIGEST_STRING_LENGTH];
 
-	len = BN_num_bytes(shared_s);
-	if ((buf = malloc(len+1)) == NULL)
-		goto fail;
+	generate_hmac(hmac, server);
 
-	BN_bn2bin(shared_s, buf);
-
-	if ((res = SHA256Data(buf, len, NULL)) == NULL)
+	if ((buf = srecv(connfd)) == NULL ||
+	    ssend(connfd, strcmp(buf, hmac) == 0 ? "OK" : "NO") == 0)
 		goto fail;
 
 	free(buf);
-	return res;
+	return 1;
 fail:
-	return NULL;
+	return 0;
 }
 
 int
 main(void)
 {
+	struct state server;
 	int listenfd, connfd;
+	BIGNUM *client_pub_key;
 	pid_t pid;
-	char *buf, *p;
-	size_t i;
 
-	if (init_params(&modulus, &generator, &multiplier) == 0 ||
-	    (salt = make_salt()) == NULL ||
-	    (verifier = make_verifier(salt)) == NULL ||
-	    (private_key = make_private_key()) == NULL ||
-	    (public_key = make_public_key(multiplier, verifier, generator, private_key, modulus)) == NULL ||
-	    (listenfd = lo_listen(PORT)) == -1)
+	if (server_init(&server) == 0 ||
+	    (listenfd = lo_listen(PORT)) == 0 ||
+	    (client_pub_key = BN_new()) == NULL)
 		err(1, NULL);
 
 	for (;;) {
@@ -264,47 +312,12 @@ main(void)
 		}
 		close(listenfd);
 
-		if ((buf = srecv(connfd)) == NULL)
+		if (get_username_and_client_pub_key(connfd, &server, &client_pub_key) == 0 ||
+		    send_salt_and_server_pub_key(connfd, &server) == 0 ||
+		    server_generate_enc_key(&server, client_pub_key) == 0 ||
+		    server_verify_hmac(connfd, &server) == 0)
 			err(1, NULL);
 
-		p = buf;
-		if ((i = strcspn(p, " ")) > strlen(p)-2)
-			errx(1, "invalid username");
-		p[i] = '\0';
-		if (strcmp(p, USERNAME) != 0)
-			errx(1, "invalid username");
-
-		p += i+1;
-		if ((client_pubkey = BN_new()) == NULL ||
-		    BN_hex2bn(&client_pubkey, p) == 0)
-			err(1, NULL);
-
-		free(buf);
-
-		if ((buf = BN_bn2hex(public_key)) == NULL ||
-		    ssendf(connfd, "%s %s", salt, buf) == 0)
-			err(1, NULL);
-
-		free(buf);
-
-		if ((scrambler = make_scrambler(client_pubkey, public_key)) == NULL ||
-		    (shared_s = make_shared_s(client_pubkey, verifier, scrambler, private_key, modulus)) == NULL ||
-		    (shared_k = make_shared_k(shared_s)) == NULL ||
-		    (hmac = make_hmac(shared_k, salt)) == NULL ||
-
-		    (buf = srecv(connfd)) == NULL ||
-		    ssend(connfd, strcmp(buf, hmac) == 0 ? "OK" : "NO") == 0)
-			err(1, NULL);
-
-		break;
+		exit(0);
 	}
-
-	exit(0);
-}
-*/
-
-int
-main(void)
-{
-	return 0;
 }
