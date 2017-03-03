fd9d76424946d4aa7faa7eca690a8c80c11c4c8b
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-03-02 22:54:33 -0500

    set6

--- set6/44/util.c
+++ set6/44/util.c
@@ -1,4 +1,5 @@
-#include <err.h>
+#include <sys/types.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,48 +7,38 @@
 #include <openssl/bn.h>
 #include <openssl/dsa.h>
 
-#define FILENAME "DATA"
+#include "44.h"
 
-struct entry {
-	BIGNUM *m;
-	DSA_SIG *sig;
-};
-
-struct {
-	struct entry **entries;
-	size_t len;
-} data;
-
-struct entry *
-entry_new(char *s_buf, char *r_buf, char *m_buf)
+static struct entry *
+new_entry(char *s_buf, char *r_buf, char *m_buf)
 {
 	struct entry *entry;
-	size_t span;
+	size_t i;
 
 	if ((entry = malloc(sizeof(*entry))) == NULL ||
 	    (entry->m = BN_new()) == NULL ||
 	    (entry->sig = DSA_SIG_new()) == NULL)
 		goto fail;
 
-	if ((span = strcspn(m_buf, " ")) > strlen(m_buf)-2)
+	if ((i = strcspn(m_buf, " ")) > strlen(m_buf)-2)
 		goto fail;
-	m_buf += span + 1;
+	m_buf += i+1;
 
 	m_buf[strcspn(m_buf, "\n")] = '\0';
 	if (BN_hex2bn(&entry->m, m_buf) == 0)
 		goto fail;
 
-	if ((span = strcspn(s_buf, " ")) > strlen(s_buf)-2)
+	if ((i = strcspn(s_buf, " ")) > strlen(s_buf)-2)
 		goto fail;
-	s_buf += span + 1;
+	s_buf += i+1;
 
 	s_buf[strcspn(s_buf, "\n")] = '\0';
 	if (BN_dec2bn(&entry->sig->s, s_buf) == 0)
 		goto fail;
 
-	if ((span = strcspn(r_buf, " ")) > strlen(r_buf)-2)
+	if ((i = strcspn(r_buf, " ")) > strlen(r_buf)-2)
 		goto fail;
-	r_buf += span + 1;
+	r_buf += i+1;
 
 	r_buf[strcspn(r_buf, "\n")] = '\0';
 	if (BN_dec2bn(&entry->sig->r, r_buf) == 0)
@@ -59,44 +50,32 @@ fail:
 }
 
 int
-load_data(FILE *fp)
+load_data(struct data *data, FILE *fp)
 {
 	char *msg_buf, *s_buf, *r_buf, *m_buf;
 	size_t msg_size, s_size, r_size, m_size;
 	ssize_t len;
 	struct entry *entry, **newp;
 
+	data->entries = NULL;
+	data->len = 0;
+
 	msg_buf = s_buf = r_buf = m_buf = NULL;
 	msg_size = s_size = r_size = m_size = 0;
 
 	for (;;) {
-		/*
-		if (getline(&msg_buf, &msg_size, fp) == -1 ||
-
-		    (len = getline(&s_buf, &s_size, fp)) == -1)
-			break;
-		s_buf[len] = '\0';
-
-		if ((len = getline(&r_buf, &r_size, fp)) == -1)
-			break;
-		r_buf[len] = '\0';
-
-		if ((len = getline(&m_buf, &m_size, fp)) == -1)
-			break;
-		m_buf[len] = '\0';
-		*/
 		if (getline(&msg_buf, &msg_size, fp) == -1 ||
 		    getline(&s_buf, &s_size, fp) == -1 ||
 		    getline(&r_buf, &r_size, fp) == -1 ||
 		    getline(&m_buf, &m_size, fp) == -1)
 			break;
 
-		if ((entry = entry_new(s_buf, r_buf, m_buf)) == NULL ||
-		    (newp = reallocarray(data.entries, data.len+1, sizeof(*data.entries))) == NULL)
+		if ((entry = new_entry(s_buf, r_buf, m_buf)) == NULL ||
+		    (newp = reallocarray(data->entries, data->len+1, sizeof(*data->entries))) == NULL)
 			goto fail;
 
-		data.entries = newp;
-		data.entries[data.len++] = entry;
+		data->entries = newp;
+		data->entries[data->len++] = entry;
 	}
 
 	if (ferror(fp))
@@ -110,14 +89,4 @@ load_data(FILE *fp)
 	return 1;
 fail:
 	return 0;
-}
-
-int
-main(void)
-{
-	FILE *fp;
-
-	if ((fp = fopen(FILENAME, "r")) == NULL ||
-	    load_data(fp) == 0)
-		err(1, NULL);
 }
