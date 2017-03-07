6cbe5a37de5f6f4134a967f5c8ce133da4ac0267
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-03-06 20:05:59 -0500

    46 done

--- set6/46/46.c
+++ set6/46/46.c
@@ -70,25 +70,63 @@ print_hollywood_style(char *buf, size_t len)
 			putchar(c);
 		else
 			putchar('?');
+	putchar('\n');
 }
 
-char *
+int
 crack_rsa(RSA *rsa, BIGNUM *enc)
 {
 	BN_CTX *ctx;
-	size_t i, len;
-	int even;
+	BIGNUM *tmp, *two, *factor, *lower, *upper;
 	char *buf;
+	int even;
 
 	if ((ctx = BN_CTX_new()) == NULL)
 		goto fail;
 	BN_CTX_start(ctx);
 
-	for (i = 0; i < BITS; i++) {
+	if ((tmp = BN_CTX_get(ctx)) == NULL ||
+	    (two = BN_CTX_get(ctx)) == NULL ||
+	    (factor = BN_CTX_get(ctx)) == NULL ||
+	    (lower = BN_CTX_get(ctx)) == NULL ||
+	    (upper = BN_CTX_get(ctx)) == NULL ||
+
+	    BN_copy(tmp, enc) == 0 ||
+	    BN_set_word(two, 2) == 0 ||
+	    BN_mod_exp(factor, two, rsa->e, rsa->n, ctx) == 0 ||
+	    BN_zero(lower) == 0 ||
+	    BN_copy(upper, rsa->n) == 0 ||
+
+	    (buf = malloc(RSA_size(rsa))) == NULL)
+		goto fail;
+
+	while (BN_cmp(lower, upper)) {
+		if (BN_mod_mul(tmp, tmp, factor, rsa->n, ctx) == 0 ||
+		    (even = is_plaintext_even(rsa, tmp, ctx)) == -1)
+			goto fail;
+
+		if (even) {
+			if (BN_add(upper, lower, upper) == 0 ||
+			    BN_div(upper, NULL, upper, two, ctx) == 0)
+				goto fail;
+		} else {
+			if (BN_add(lower, lower, upper) == 0 ||
+			    BN_div(lower, NULL, lower, two, ctx) == 0)
+				goto fail;
+		}
+
+		if (BN_bn2bin(upper, buf) == 0)
+			goto fail;
+		print_hollywood_style(buf, BN_num_bytes(upper));
 	};
 
+	BN_CTX_end(ctx);
+	BN_CTX_free(ctx);
+	free(buf);
+
+	return 1;
 fail:
-	return NULL;
+	return 0;
 }
 
 int
@@ -103,10 +141,10 @@ main(void)
 	    BN_set_word(f4, RSA_F4) == 0 ||
 	    RSA_generate_key_ex(rsa, BITS, f4, NULL) == 0 ||
 
-	    (enc = rsa_encrypt_b64(rsa, (char *) data)) == NULL)
-		err(1, NULL);
+	    (enc = rsa_encrypt_b64(rsa, (char *) data)) == NULL ||
 
-	crack_rsa(rsa, enc);
+	    crack_rsa(rsa, enc) == 0)
+		err(1, NULL);
 
 	exit(0);
 }
