b2af55ea9a912100f699cf16ebe68b5e8c92b774
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-03-07 21:57:29 -0500

    encrypt

--- set6/47/47.c
+++ set6/47/47.c
@@ -1,3 +1,8 @@
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include <openssl/bn.h>
 #include <openssl/rsa.h>
 
@@ -5,10 +10,18 @@
 
 const char *data = "kick it, CC";
 
-char *
+uint8_t *
 rsa_encrypt(RSA *rsa, char *buf)
 {
+	uint8_t *res;
 
+	if ((res = malloc(RSA_size(rsa))) == NULL ||
+	    RSA_public_encrypt(strlen(buf), buf, res, rsa, RSA_PKCS1_PADDING) == 0)
+		goto fail;
+
+	return res;
+fail:
+	return NULL;
 }
 
 int
@@ -20,5 +33,15 @@ check_padding(RSA *rsa, char *enc)
 int
 main(void)
 {
-	return 0;
+	RSA *rsa;
+	BIGNUM *f4;
+
+	if ((rsa = RSA_new()) == NULL ||
+	    (f4 = BN_new()) == NULL ||
+
+	    BN_set_word(f4, RSA_F4) == 0 ||
+	    RSA_generate_key_ex(rsa, BITS, f4, NULL) == 0)
+		err(1, NULL);
+
+	rsa_encrypt(rsa, "hello world");
 }
