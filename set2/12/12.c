88ed36e31b9ceff73acfb20f0ec6e06edcf46994
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-03-17 18:48:06 -0400

    array to pointer

--- set2/12/12.c
+++ set2/12/12.c
@@ -13,7 +13,7 @@
 uint8_t *
 encrypt(uint8_t *in, size_t inlen, size_t *outlenp)
 {
-	const char secret[] =
+	const char *secret =
 	    "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg"
 	    "aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq"
 	    "dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg"
