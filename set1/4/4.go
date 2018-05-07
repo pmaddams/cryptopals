d45aa11194f9687225415b7e1164bccfef9ff458
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-05-07 19:21:23 -0400

    4

---
+++ set1/4/4.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"bufio"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+const sample = "alice.txt"
+
+// Symbols reads text and returns a map of UTF-8 symbol frequencies.
+func Symbols(in io.Reader) (map[rune]float64, error) {
+	buf, err := ioutil.ReadAll(in)
+	if err != nil {
+		return nil, err
+	}
+	runes := []rune(string(buf))
+	m := make(map[rune]float64)
+
+	for _, r := range runes {
+		m[r] += 1.0 / float64(len(runes))
+	}
+	return m, nil
+}
+
+// Score adds up the frequencies for UTF-8 symbols encoded in the buffer.
+func Score(m map[rune]float64, buf []byte) (res float64) {
+	runes := []rune(string(buf))
+	for _, r := range runes {
+		f, _ := m[r]
+		res += f
+	}
+	return
+}
+
+// XORByte produces the XOR combination of a buffer with a single byte.
+func XORByte(out, buf []byte, b byte) int {
+	n := len(buf)
+	for i := 0; i < n; i++ {
+		out[i] = buf[i] ^ b
+	}
+	return n
+}
+
+// allXORByteBuffers returns all single-byte XOR products of a buffer.
+func allXORByteBuffers(buf []byte) (res [256][]byte) {
+	for i := 0; i < len(res); i++ {
+		res[i] = make([]byte, len(buf))
+		XORByte(res[i], buf, byte(i))
+	}
+	return
+}
+
+// bestXORByteBuffer takes a buffer and a scoring function, and returns
+// the message and its score.
+func bestXORByteBuffer(buf []byte, scoreFunc func([]byte) float64) ([]byte, float64) {
+	var best float64
+	var msg []byte
+
+	for _, try := range allXORByteBuffers(buf) {
+		if score := scoreFunc(try); score > best {
+			best = score
+			msg = try
+		}
+	}
+	return msg, best
+}
+
+// detectXORByteCipher reads hex-encoded data and prints the string (if any)
+// which has been encrypted with single-byte XOR.
+func detectXORByteCipher(in io.Reader, scoreFunc func([]byte) float64) {
+	input := bufio.NewScanner(in)
+	var best float64
+	var msg []byte
+
+	for input.Scan() {
+		buf, err := hex.DecodeString(input.Text())
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			return
+		}
+		if try, score := bestXORByteBuffer(buf, scoreFunc); score > best {
+			best = score
+			msg = try
+		}
+	}
+	if err := input.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+	}
+	fmt.Print(string(msg))
+}
+
+func main() {
+	// Generate a scoring function from a sample file.
+	scoreFunc := func() func([]byte) float64 {
+		var f *os.File
+		var err error
+
+		f, err = os.Open(sample)
+		defer f.Close()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		var m map[rune]float64
+		m, err = Symbols(f)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		return func(buf []byte) float64 {
+			return Score(m, buf)
+		}
+	}()
+
+	files := os.Args[1:]
+	// If no files are specified, read from standard input.
+	if len(files) == 0 {
+		detectXORByteCipher(os.Stdin, scoreFunc)
+		return
+	}
+	for _, name := range files {
+		f, err := os.Open(name)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			continue
+		}
+		detectXORByteCipher(f, scoreFunc)
+		f.Close()
+	}
+}
---
+++ set1/4/4_test.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bytes"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestSymbols(t *testing.T) {
+	cases := []struct {
+		s    string
+		want map[rune]float64
+	}{
+		{
+			"hello world",
+			map[rune]float64{
+				'h': 1.0 / 11.0,
+				'e': 1.0 / 11.0,
+				'l': 3.0 / 11.0,
+				'o': 2.0 / 11.0,
+				' ': 1.0 / 11.0,
+				'w': 1.0 / 11.0,
+				'r': 1.0 / 11.0,
+				'd': 1.0 / 11.0,
+			},
+		},
+	}
+	for _, c := range cases {
+		got, _ := Symbols(strings.NewReader(c.s))
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("Symbols(%v) == %v, want %v",
+				c.s, got, c.want)
+		}
+	}
+}
+
+func symbols(s string) map[rune]float64 {
+	m, _ := Symbols(strings.NewReader(s))
+	return m
+}
+
+func TestScore(t *testing.T) {
+	cases := []struct {
+		m    map[rune]float64
+		s    string
+		want float64
+	}{
+		{
+			symbols("hello world"),
+			"hola",
+			1.0/11.0 + 2.0/11.0 + 3.0/11.0,
+		},
+		{
+			symbols("你好世界"),
+			"世界再见",
+			1.0/4.0 + 1.0/4.0,
+		},
+	}
+	for _, c := range cases {
+		got := Score(c.m, []byte(c.s))
+		if got != c.want {
+			t.Errorf("Score(%v) == %v, want %v",
+				c.s, got, c.want)
+		}
+	}
+}
+
+func TestXORByte(t *testing.T) {
+	cases := []struct {
+		out, buf []byte
+		b        byte
+		want     []byte
+	}{
+		{
+			make([]byte, 11),
+			[]byte("hello world"),
+			0,
+			[]byte("hello world"),
+		},
+		{
+			make([]byte, 4),
+			[]byte{0, 0, 0, 0},
+			1,
+			[]byte{1, 1, 1, 1},
+		},
+	}
+	for _, c := range cases {
+		XORByte(c.out, c.buf, c.b)
+		if !bytes.Equal(c.out, c.want) {
+			t.Errorf("XORByte(%v, %v, %v), want %v",
+				c.out, c.buf, c.b, c.want)
+		}
+	}
+}
