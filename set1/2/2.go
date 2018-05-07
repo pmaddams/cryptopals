81d01ae47f0c3924d3f0278197f42bc4da7ce7a7
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-05-07 15:26:13 -0400

    2

--- set1/2/2.go
+++ set1/2/2.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bufio"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+)
+
 // min returns the smaller of two integers.
 func min(n, m int) int {
 	if n < m {
@@ -17,5 +25,52 @@ func XORBytes(dst, b1, b2 []byte) int {
 	return n
 }
 
+// xorTwoLines reads two hex-encoded lines and prints their XOR combination.
+func xorTwoLines(r io.Reader) {
+	input := bufio.NewScanner(r)
+	var b1, b2, out []byte
+	var err error
+
+	// Read two lines and exit if either one is empty or invalid.
+	if !input.Scan() || len(input.Text()) == 0 {
+		return
+	}
+	if b1, err = hex.DecodeString(input.Text()); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		return
+	}
+	if !input.Scan() || len(input.Text()) == 0 {
+		return
+	}
+	if b2, err = hex.DecodeString(input.Text()); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		return
+	}
+
+	// Write the data in place to the shorter buffer.
+	if len(b1) < len(b2) {
+		out = b1
+	} else {
+		out = b2
+	}
+	XORBytes(out, b1, b2)
+	fmt.Printf("%x\n", out)
+}
+
 func main() {
+	files := os.Args[1:]
+	// If no files are specified, read from standard input.
+	if len(files) == 0 {
+		xorTwoLines(os.Stdin)
+		return
+	}
+	for _, arg := range files {
+		f, err := os.Open(arg)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			continue
+		}
+		xorTwoLines(f)
+		f.Close()
+	}
 }
--- set1/2/2_test.go
+++ set1/2/2_test.go
@@ -35,7 +35,7 @@ func TestXORBytes(t *testing.T) {
 	for _, c := range cases {
 		got = make([]byte, len(c.b1))
 		if XORBytes(got, c.b1, c.b2); !bytes.Equal(got, c.want) {
-			t.Errorf("Xor(%v, %v) == %v, want %v",
+			t.Errorf("XORBytes(%v, %v) == %v, want %v",
 				c.b1, c.b2, got, c.want)
 		}
 	}
