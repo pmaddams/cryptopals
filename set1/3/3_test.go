ef44718a4036ce29734b4b35d1ff9f856873070c
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-05-07 16:06:27 -0400

    3

--- set1/3/3_test.go
+++ set1/3/3_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"reflect"
 	"strings"
 	"testing"
@@ -58,6 +59,40 @@ func TestScore(t *testing.T) {
 		if got != c.want {
 			t.Errorf("Score(%v) == %v, want %v",
 				c.s, got, c.want)
+		}
+	}
+}
+
+func TestXORByte(t *testing.T) {
+	cases := []struct {
+		got, bytes []byte
+		b          byte
+		want       []byte
+	}{
+		{
+			make([]byte, 11),
+			[]byte("hello world"),
+			0,
+			[]byte("hello world"),
+		},
+		{
+			make([]byte, 4),
+			[]byte{0, 0, 0, 0},
+			1,
+			[]byte{1, 1, 1, 1},
+		},
+		{
+			make([]byte, 4),
+			[]byte{12, 34, 56, 78},
+			90,
+			[]byte{12 ^ 90, 34 ^ 90, 56 ^ 90, 78 ^ 90},
+		},
+	}
+	for _, c := range cases {
+		XORByte(c.got, c.bytes, c.b)
+		if !bytes.Equal(c.got, c.want) {
+			t.Errorf("XORByte(%v, %v, %v), want %v",
+				c.got, c.bytes, c.b, c.want)
 		}
 	}
 }
