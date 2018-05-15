1541ab4ab2fad7f1eaeb3dd6c7fa1e5866f69cfc
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-05-15 17:58:44 -0400

    11

--- set2/11/11.go
+++ set2/11/11.go
@@ -28,6 +28,8 @@ func (mode ecbEncrypter) BlockSize() int {
 
 // CryptBlocks encrypts a buffer in ECB mode.
 func (mode ecbEncrypter) CryptBlocks(dst, src []byte) {
+	// The src buffer length must be a multiple of the block size,
+	// and the dst buffer must be at least the length of src.
 	for n := mode.BlockSize(); len(src) > 0; {
 		mode.b.Encrypt(dst[:n], src[:n])
 		dst = dst[n:]
