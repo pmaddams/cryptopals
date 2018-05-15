fcd15e27573876573e9f58a3ae23f93a46815e17
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-05-14 20:00:51 -0400

    7

--- set1/7/7.go
+++ set1/7/7.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/base64"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -21,9 +23,12 @@ func (x ecb) BlockSize() int {
 	return x.b.BlockSize()
 }
 
-// cryptBlocks unsafely attempts to operate on multiple blocks.
+// cryptBlocks encrypts or decrypts multiple blocks.
 func (x ecb) cryptBlocks(dst, src []byte, crypt func([]byte, []byte)) {
-	for n := x.BlockSize(); len(src) >= n; {
+	if len(src)%x.BlockSize() != 0 {
+		panic("cryptBlocks: input not full blocks")
+	}
+	for n := x.BlockSize(); len(src) > 0; {
 		crypt(dst[:n], src[:n])
 		dst = dst[n:]
 		src = src[n:]
@@ -33,14 +38,12 @@ func (x ecb) cryptBlocks(dst, src []byte, crypt func([]byte, []byte)) {
 // ecbEncrypter embeds ecb.
 type ecbEncrypter struct{ ecb }
 
-// NewECBEncrypter returns a cipher.BlockMode that encrypts in ECB mode.
+// NewECBEncrypter returns a block mode for ECB encryption.
 func NewECBEncrypter(block cipher.Block) cipher.BlockMode {
 	return ecbEncrypter{ecb{block}}
 }
 
-// ecbEncrypter.CryptBlocks implements ECB encryption for multiple blocks.
-// In this case, it intentionally violates the cipher.BlockMode specification
-// by allowing the source buffer to not be a multiple of the block size.
+// ecbEncrypter.CryptBlocks encrypts multiple blocks.
 func (mode ecbEncrypter) CryptBlocks(dst, src []byte) {
 	mode.cryptBlocks(dst, src, mode.b.Encrypt)
 }
@@ -48,16 +51,42 @@ func (mode ecbEncrypter) CryptBlocks(dst, src []byte) {
 // ecbDecrypter embeds ecb.
 type ecbDecrypter struct{ ecb }
 
-// NewECBDecrypter returns a BlockMode that decrypts in ECB mode.
+// NewECBEncrypter returns a block mode for ECB decryption.
 func NewECBDecrypter(block cipher.Block) cipher.BlockMode {
 	return ecbDecrypter{ecb{block}}
 }
 
-// ecbDecrypter.CryptBlocks implements ECB decryption for multiple blocks.
-// In this case, it intentionally violates the cipher.BlockMode specification
-// by allowing the source buffer to not be a multiple of the block size.
+// ecbDecrypter.CryptBlocks decrypts multiple blocks.
 func (mode ecbDecrypter) CryptBlocks(dst, src []byte) {
 	mode.cryptBlocks(dst, src, mode.b.Decrypt)
+}
+
+// PKCS7Pad returns a copy of a buffer with PKCS#7 padding.
+func PKCS7Pad(buf []byte, blockSize int) []byte {
+	var n int
+
+	// If the buffer length is a multiple of the block size,
+	// add a number of padding bytes equal to the block size.
+	if rem := len(buf) % blockSize; rem == 0 {
+		n = blockSize
+	} else {
+		n = blockSize - rem
+	}
+	for i := 0; i < n; i++ {
+		buf = append(buf, byte(n))
+	}
+	return buf
+}
+
+// PKCS7Unpad returns a copy of a buffer with PKCS#7 padding removed.
+func PKCS7Unpad(buf []byte, blockSize int) ([]byte, error) {
+	// Examine the value of the last byte.
+	b := buf[len(buf)-1]
+	if int(b) > blockSize ||
+		!bytes.Equal(bytes.Repeat([]byte{b}, int(b)), buf[len(buf)-int(b):]) {
+		return nil, errors.New("PKCS7Unpad: invalid padding")
+	}
+	return buf[:len(buf)-int(b)], nil
 }
 
 // encryptAndPrint reads plaintext and prints base64-encoded ciphertext.
@@ -67,19 +96,26 @@ func encryptAndPrint(in io.Reader, block cipher.Block) {
 		fmt.Fprintln(os.Stderr, err.Error())
 		return
 	}
+	buf = PKCS7Pad(buf, block.BlockSize())
 	NewECBEncrypter(block).CryptBlocks(buf, buf)
 	fmt.Println(base64.StdEncoding.EncodeToString(buf))
 }
 
 // decryptAndPrint reads base64-encoded ciphertext and prints plaintext.
 func decryptAndPrint(in io.Reader, block cipher.Block) {
-	buf, err := ioutil.ReadAll(base64.NewDecoder(base64.StdEncoding, in))
-	if err != nil {
+	in = base64.NewDecoder(base64.StdEncoding, in)
+	var buf []byte
+	var err error
+	if buf, err = ioutil.ReadAll(in); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		return
 	}
 	NewECBDecrypter(block).CryptBlocks(buf, buf)
-	fmt.Println(string(buf))
+	if buf, err = PKCS7Unpad(buf, block.BlockSize()); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		return
+	}
+	fmt.Print(string(buf))
 }
 
 var e = flag.Bool("e", false, "encrypt")
