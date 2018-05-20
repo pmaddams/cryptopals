c65af110959ebca11b3d42c61df5fd66844abac8
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-05-20 18:49:41 -0400

    16

---
+++ set2/16/16.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"errors"
+)
+
+// AES always has a block size of 128 bits (16 bytes).
+const aesBlockSize = 16
+
+// RandomCipher returns an AES cipher with a random key.
+func RandomCipher() cipher.Block {
+	key := make([]byte, aesBlockSize)
+	if _, err := rand.Read(key); err != nil {
+		panic(err.Error())
+	}
+	block, _ := aes.NewCipher(key)
+	return block
+}
+
+// PKCS7Pad returns a buffer with PKCS#7 padding added.
+func PKCS7Pad(buf []byte, blockSize int) []byte {
+	var n int
+
+	// If the buffer length is a multiple of the block size,
+	// add a number of padding bytes equal to the block size.
+	if rem := len(buf) % blockSize; rem == 0 {
+		n = blockSize
+	} else {
+		n = blockSize - rem
+	}
+	for i := 0; i < n; i++ {
+		buf = append(buf, byte(n))
+	}
+	return buf
+}
+
+// PKCS7Unpad returns a buffer with PKCS#7 padding removed.
+func PKCS7Unpad(buf []byte, blockSize int) ([]byte, error) {
+	if len(buf) < blockSize {
+		return nil, errors.New("PKCS7Unpad: invalid padding")
+	}
+	// Examine the value of the last byte.
+	b := buf[len(buf)-1]
+	if int(b) > blockSize ||
+		!bytes.Equal(bytes.Repeat([]byte{b}, int(b)), buf[len(buf)-int(b):]) {
+		return nil, errors.New("PKCS7Unpad: invalid padding")
+	}
+	return buf[:len(buf)-int(b)], nil
+}
+
+func main() {
+}
