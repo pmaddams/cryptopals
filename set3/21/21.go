f144932ec413d2e30a93dde1ce2b068843e4f0c3
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-05-28 18:37:57 -0400

    21

--- set3/21/21.go
+++ set3/21/21.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 )
@@ -15,51 +16,60 @@ const coefficient = 0x9908b0df
 const temperMask1 = 0x9d2c5680
 const temperMask2 = 0xefc60000
 
-type rand struct {
+// MT19937 contains state for the MT19937 PRNG.
+type MT19937 struct {
 	state [arraySize]uint32
 	pos   int
 }
 
-func NewRand(seed uint32) *rand {
-	var rand rand
-	rand.state[0] = seed
+// NewMT19937 initializes and returns a new PRNG.
+func NewMT19937(seed uint32) *MT19937 {
+	var mt MT19937
+	mt.state[0] = seed
 	for i := 1; i < arraySize; i++ {
-		rand.state[i] = multiplier*
-			(rand.state[i-1]^(rand.state[i-1]>>30)) +
+		mt.state[i] = multiplier*
+			(mt.state[i-1]^(mt.state[i-1]>>30)) +
 			uint32(i)
 	}
-	return &rand
+	mt.twist()
+	return &mt
 }
 
-func (rand *rand) twist() {
+// twist transforms the PRNG state array.
+func (mt *MT19937) twist() {
 	for i := 0; i < arraySize; i++ {
-		n := (rand.state[i] & upperMask) | (rand.state[(i+1)%arraySize] & lowerMask)
-		rand.state[i] = rand.state[(i+offset)%arraySize] ^ (n >> 1)
+		n := (mt.state[i] & upperMask) | (mt.state[(i+1)%arraySize] & lowerMask)
+		mt.state[i] = mt.state[(i+offset)%arraySize] ^ (n >> 1)
 		if n%2 != 0 {
-			rand.state[i] ^= coefficient
+			mt.state[i] ^= coefficient
 		}
 	}
 }
 
-func (rand *rand) Uint32() uint32 {
-	n := rand.state[rand.pos]
-	n ^= (n >> 11)
+// Uint32 returns a pseudo-random unsigned 32-bit integer.
+func (mt *MT19937) Uint32() uint32 {
+	n := mt.state[mt.pos]
+	n ^= n >> 11
 	n ^= (n << 7) & temperMask1
 	n ^= (n << 15) & temperMask2
-	n ^= (n >> 18)
+	n ^= n >> 18
 
-	rand.pos++
-	if rand.pos == arraySize {
-		rand.twist()
-		rand.pos = 0
+	mt.pos++
+	if mt.pos == arraySize {
+		mt.twist()
+		mt.pos = 0
 	}
 	return n
 }
 
 func main() {
-	rand := NewRand(5489)
+	var seed uint
+	flag.UintVar(&seed, "s", 5489, "seed")
+	flag.Parse()
+
+	mt := NewMT19937(uint32(seed))
 	input := bufio.NewScanner(os.Stdin)
 	for input.Scan() {
-		fmt.Print(rand.Uint32())
+		fmt.Print(mt.Uint32())
 	}
 }
