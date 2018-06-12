a31000c8a57d0f603d0622830c5ac741c4421e78
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-06-12 18:58:27 -0400

    29

--- set4/29/29_test.go
+++ set4/29/29_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha1"
 	"encoding/binary"
 	weak "math/rand"
 	"testing"
@@ -38,6 +39,35 @@ func TestBitPadding(t *testing.T) {
 		endian.PutUint64(tmp, uint64(n)<<3)
 		if !bytes.Equal(tmp, pad[len(pad)-8:]) {
 			fail("incorrect bit count")
+		}
+	}
+}
+
+func TestPrefixedSHA1(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		h := sha1.New()
+		buf1 := make([]byte, weak.Intn(1024))
+		weak.Read(buf1)
+		h.Write(buf1)
+		sum := h.Sum([]byte{})
+
+		pad := BitPadding(len(buf1), sha1.BlockSize, binary.BigEndian)
+
+		h.Reset()
+		buf2 := make([]byte, weak.Intn(1024))
+		weak.Read(buf2)
+		h.Write(append(buf1, append(pad, buf2...)...))
+		want := h.Sum([]byte{})
+
+		h, err := PrefixedSHA1(sum, len(buf1))
+		if err != nil {
+			t.Fatal(err)
+		}
+		h.Write(buf2)
+		got := h.Sum([]byte{})
+		if !bytes.Equal(got, want) {
+			t.Errorf("PrefixedSHA1(%x, %v) == %x, want %x",
+				sum, len(buf1), got, want)
 		}
 	}
 }
