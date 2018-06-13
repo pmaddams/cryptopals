16e0d72337175b9b9310d7615ea748d2ede2b639
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-06-13 11:43:56 -0400

    30

--- set4/30/30.go
+++ set4/30/30.go
@@ -2,42 +2,19 @@ package main
 
 import (
 	"bytes"
+	"crypto/rand"
 	"encoding/binary"
 	"errors"
 	"fmt"
 	"hash"
-	"io"
+	weak "math/rand"
+	"os"
 	"reflect"
+	"time"
 	"unsafe"
 
 	"golang.org/x/crypto/md4"
 )
-
-// PrefixedMD4 returns a new MD4 hash using an existing checksum and buffer length.
-func PrefixedMD4(sum []byte, n int) (hash.Hash, error) {
-	if len(sum) != md4.Size {
-		return nil, errors.New("PrefixedMD4: invalid checksum")
-	}
-	h := md4.New()
-
-	var newState [4]uint32
-	for i := range newState {
-		newState[i] = binary.LittleEndian.Uint32(sum[:4])
-		sum = sum[4:]
-	}
-	newLen := uint64(n - (n % md4.BlockSize) + md4.BlockSize)
-
-	// Circumvent the type system to modify unexported data structures.
-	state := reflect.ValueOf(h).Elem().Field(0)
-	state = reflect.NewAt(state.Type(), unsafe.Pointer(state.UnsafeAddr())).Elem()
-	state.Set(reflect.ValueOf(newState))
-
-	len := reflect.ValueOf(h).Elem().Field(3)
-	len = reflect.NewAt(len.Type(), unsafe.Pointer(len.UnsafeAddr())).Elem()
-	len.Set(reflect.ValueOf(newLen))
-
-	return h, nil
-}
 
 // BitPadding returns bit padding for the given buffer length.
 func BitPadding(n, blockSize int, endian binary.ByteOrder) []byte {
@@ -59,23 +36,102 @@ func BitPadding(n, blockSize int, endian binary.ByteOrder) []byte {
 	return res
 }
 
-func main() {
-	h1 := md4.New()
-	io.WriteString(h1, "hello")
-	sum1 := h1.Sum([]byte{})
+// setUnexported sets a possibly unexported value.
+func setUnexported(v1, v2 reflect.Value) {
+	reflect.NewAt(v1.Type(), unsafe.Pointer(v1.UnsafeAddr())).Elem().Set(v2)
+}
 
-	h2, err := PrefixedMD4(sum1, len("hello"))
-	if err != nil {
+// PrefixedMD4 returns a new MD4 hash using an existing checksum and buffer length.
+func PrefixedMD4(sum []byte, n int) (hash.Hash, error) {
+	if len(sum) != md4.Size {
+		return nil, errors.New("PrefixedMD4: invalid checksum")
+	}
+	h := md4.New()
+
+	var state [4]uint32
+	for i := range state {
+		state[i] = binary.LittleEndian.Uint32(sum[:4])
+		sum = sum[4:]
+	}
+	pad := BitPadding(n, md4.BlockSize, binary.LittleEndian)
+	written := uint64(n + len(pad))
+
+	setUnexported(reflect.ValueOf(h).Elem().Field(0), reflect.ValueOf(state))
+	setUnexported(reflect.ValueOf(h).Elem().Field(3), reflect.ValueOf(written))
+
+	return h, nil
+}
+
+// mac contains a hash and secret key.
+type mac struct {
+	hash.Hash
+	key []byte
+}
+
+// NewMAC takes a hash and key, and returns a new MAC hash.
+func NewMAC(h func() hash.Hash, key []byte) hash.Hash {
+	m := mac{h(), key}
+	m.Reset()
+	return m
+}
+
+// Reset resets the hash.
+func (m mac) Reset() {
+	m.Hash.Reset()
+	if _, err := m.Hash.Write(m.key); err != nil {
 		panic(err)
 	}
-	io.WriteString(h2, "world")
-	sum2 := h2.Sum([]byte{})
-	fmt.Printf("%x\n", sum2)
+}
 
-	pad := BitPadding(len("hello"), md4.BlockSize, binary.LittleEndian)
-	buf := append([]byte("hello"), append(pad, []byte("world")...)...)
-	h3 := md4.New()
-	h3.Write(buf)
-	sum3 := h3.Sum([]byte{})
-	fmt.Printf("%x\n", sum3)
+// RandomRange returns a pseudo-random non-negative integer in [lo, hi].
+// The output should not be used in a security-sensitive context.
+func RandomRange(lo, hi int) int {
+	if lo < 0 || lo > hi {
+		panic("RandomRange: invalid range")
+	}
+	weak := weak.New(weak.NewSource(time.Now().UnixNano()))
+	return lo + weak.Intn(hi-lo+1)
+}
+
+// RandomBytes returns a random buffer of the desired length.
+func RandomBytes(n int) []byte {
+	res := make([]byte, n)
+	if _, err := rand.Read(res); err != nil {
+		panic(err)
+	}
+	return res
+}
+
+func main() {
+	const (
+		prefix = "comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon"
+		suffix = ";admin=true"
+	)
+	key := RandomBytes(RandomRange(8, 64))
+	mac := NewMAC(md4.New, key)
+
+	fmt.Fprint(mac, prefix)
+	sum := mac.Sum([]byte{})
+
+	// Guess the key length.
+	for n := 8; n <= 64; n++ {
+		h, err := PrefixedMD4(sum, n+len(prefix))
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return
+		}
+		fmt.Fprint(h, suffix)
+		guess := h.Sum([]byte{})
+
+		pad := BitPadding(n+len(prefix), md4.BlockSize, binary.LittleEndian)
+
+		mac.Reset()
+		fmt.Fprintf(mac, "%s%s%s", prefix, pad, suffix)
+		check := mac.Sum([]byte{})
+
+		if bytes.Equal(guess, check) {
+			fmt.Printf("guess: %x\ncheck: %x\n", guess, check)
+			return
+		}
+	}
 }
---
+++ set4/30/30_test.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	weak "math/rand"
+	"testing"
+	"time"
+
+	"golang.org/x/crypto/md4"
+)
+
+func TestBitPadding(t *testing.T) {
+	weak := weak.New(weak.NewSource(time.Now().UnixNano()))
+	for i := 0; i < 10; i++ {
+		n := weak.Intn(1024)
+		blockSize := 8 * (1 + weak.Intn(16))
+		var endian binary.ByteOrder
+		switch weak.Intn(2) {
+		case 0:
+			endian = binary.LittleEndian
+		default:
+			endian = binary.BigEndian
+		}
+		pad := BitPadding(n, blockSize, endian)
+		fail := func(s string) {
+			t.Errorf("BitPadding(%v, %v, %v) == %v, %s",
+				n, blockSize, endian, pad, s)
+		}
+		if len(pad) < 8 {
+			fail("padding too short")
+		}
+		if (n+len(pad))%blockSize != 0 {
+			fail("padded length not a multiple of the block size")
+		}
+		if pad[0] != 0x80 {
+			fail("invalid first padding byte")
+		}
+		tmp := make([]byte, 8)
+		endian.PutUint64(tmp, uint64(n)<<3)
+		if !bytes.Equal(tmp, pad[len(pad)-8:]) {
+			fail("incorrect bit count")
+		}
+	}
+}
+
+func TestPrefixedMD4(t *testing.T) {
+	weak := weak.New(weak.NewSource(time.Now().UnixNano()))
+	for i := 0; i < 10; i++ {
+		h := md4.New()
+		buf1 := make([]byte, weak.Intn(1024))
+		weak.Read(buf1)
+		h.Write(buf1)
+		sum := h.Sum([]byte{})
+
+		pad := BitPadding(len(buf1), md4.BlockSize, binary.LittleEndian)
+
+		h.Reset()
+		buf2 := make([]byte, weak.Intn(1024))
+		weak.Read(buf2)
+		h.Write(append(buf1, append(pad, buf2...)...))
+		want := h.Sum([]byte{})
+
+		h, err := PrefixedMD4(sum, len(buf1))
+		if err != nil {
+			t.Fatal(err)
+		}
+		h.Write(buf2)
+		got := h.Sum([]byte{})
+		if !bytes.Equal(got, want) {
+			t.Errorf("PrefixedMD4(%x, %v) == %x, want %x",
+				sum, len(buf1), got, want)
+		}
+	}
+}
+
+func TestRandomRange(t *testing.T) {
+	cases := []struct {
+		lo, hi int
+	}{
+		{0, 0},
+		{5, 10},
+		{20, 30},
+	}
+	for _, c := range cases {
+		for i := 0; i < 100; i++ {
+			got := RandomRange(c.lo, c.hi)
+			if got < c.lo || got > c.hi {
+				t.Errorf("RandomRange(%v, %v) == %v, value out of range",
+					c.lo, c.hi, got)
+			}
+		}
+	}
+}
+
+func TestRandomBytes(t *testing.T) {
+	weak := weak.New(weak.NewSource(time.Now().UnixNano()))
+	n := weak.Intn(1024)
+
+	var cases [][]byte
+	for i := 0; i < 5; i++ {
+		buf := RandomBytes(n)
+		if len(buf) != n {
+			t.Errorf("RandomBytes(%v) == %v, length %v",
+				n, buf, len(buf))
+		}
+		cases = append(cases, buf)
+		for j := 0; j < i; j++ {
+			if bytes.Equal(cases[i], cases[j]) {
+				t.Errorf("RandomBytes created identical buffers %v and %v",
+					cases[i], cases[j])
+			}
+		}
+	}
+}
