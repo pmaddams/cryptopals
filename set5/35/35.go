a1fd26686bf232334f2aff14b874ae042912670c
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-06-20 19:51:33 -0400

    35

--- set5/35/35.go
+++ set5/35/35.go
@@ -14,7 +14,7 @@ import (
 )
 
 const (
-	defaultPrime = `ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024
+	defaultP = `ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024
 e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd
 3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec
 6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f
@@ -22,31 +22,30 @@ e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd
 c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552
 bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff
 fffffffffffff`
-	defaultGenerator = `2`
+	defaultG = `2`
 )
 
 var (
-	zero          *big.Int
-	one           *big.Int
-	prime         *big.Int
-	primeMinusOne *big.Int
-	generator     *big.Int
+	zero = big.NewInt(0)
+	one  = big.NewInt(1)
 )
 
-// DHPrivateKey contains a key pair.
+// DHPrivateKey contains a prime, generator, and key pair.
 type DHPrivateKey struct {
-	pub crypto.PublicKey
+	p   *big.Int
+	g   *big.Int
 	n   *big.Int
+	pub crypto.PublicKey
 }
 
-// DHGenerateKey generates a key pair.
-func DHGenerateKey() *DHPrivateKey {
-	n, err := rand.Int(rand.Reader, prime)
+// DHGenerateKey generates a private key.
+func DHGenerateKey(p, g *big.Int) *DHPrivateKey {
+	n, err := rand.Int(rand.Reader, p)
 	if err != nil {
 		panic(err)
 	}
-	pub := crypto.PublicKey(new(big.Int).Exp(generator, n, prime))
-	return &DHPrivateKey{pub, n}
+	pub := crypto.PublicKey(new(big.Int).Exp(g, n, p))
+	return &DHPrivateKey{p, g, n, pub}
 }
 
 // Public returns the public key.
@@ -56,7 +55,7 @@ func (priv *DHPrivateKey) Public() crypto.PublicKey {
 
 // Secret takes a public key and returns a shared secret.
 func (priv *DHPrivateKey) Secret(pub crypto.PublicKey) []byte {
-	return new(big.Int).Exp(pub.(*big.Int), priv.n, prime).Bytes()
+	return new(big.Int).Exp(pub.(*big.Int), priv.n, priv.p).Bytes()
 }
 
 // RandomBytes returns a random buffer of the desired length.
@@ -113,7 +112,7 @@ func newBot() *bot {
 
 // connect initiates Diffie-Hellman key exchange from one bot to another.
 func (sender *bot) connect(receiver *bot, pub crypto.PublicKey) {
-	receiver.DHPrivateKey = DHGenerateKey()
+	receiver.DHPrivateKey = DHGenerateKey(sender.p, sender.g)
 
 	receiver.key = make([]byte, aes.BlockSize)
 	array := sha1.Sum(receiver.Secret(pub))
@@ -157,9 +156,10 @@ func equal(n, m *big.Int) bool {
 	return n.Cmp(m) == 0
 }
 
-func simulateMITM() {
+// simulateMITM simulates a man-in-the-middle attack.
+func simulateMITM(p, g *big.Int) {
 	alice, bob, mallory := newBot(), newBot(), newBot()
-	alice.DHPrivateKey = DHGenerateKey()
+	alice.DHPrivateKey = DHGenerateKey(p, g)
 
 	alice.connect(mallory, alice.Public())
 	mallory.connect(bob, alice.Public())
@@ -168,20 +168,17 @@ func simulateMITM() {
 	mallory.accept(alice, bob.Public())
 
 	var secret *big.Int
-	switch {
-	case equal(generator, one):
+	if equal(g, one) {
 		secret = one
-	case equal(generator, prime):
+	} else if equal(g, p) {
 		secret = zero
-	case equal(generator, primeMinusOne):
+	} else if pMinusOne := new(big.Int).Sub(p, one); equal(g, pMinusOne) {
 		if equal(alice.Public().(*big.Int), one) ||
 			equal(bob.Public().(*big.Int), one) {
 			secret = one
 		} else {
-			secret = primeMinusOne
+			secret = pMinusOne
 		}
-	default:
-		panic("simulateMITM: invalid generator")
 	}
 	array := sha1.Sum(secret.Bytes())
 	copy(mallory.key, array[:])
@@ -196,27 +193,12 @@ func simulateMITM() {
 	fmt.Println(mallory.buf.String())
 }
 
-func init() {
-	zero = big.NewInt(0)
-	one = big.NewInt(1)
-
-	var ok bool
-	if prime, ok = new(big.Int).SetString(strings.Replace(defaultPrime, "\n", "", -1), 16); !ok {
+func main() {
+	p, ok := new(big.Int).SetString(strings.Replace(defaultP, "\n", "", -1), 16)
+	if !ok || !p.ProbablyPrime(0) {
 		panic("invalid prime")
 	}
-	primeMinusOne = new(big.Int).Sub(prime, one)
-	if generator, ok = new(big.Int).SetString(defaultGenerator, 16); !ok {
-		panic("invalid generator")
-	}
-}
-
-func main() {
-	generator = one
-	simulateMITM()
-
-	generator = prime
-	simulateMITM()
-
-	generator = primeMinusOne
-	simulateMITM()
+	simulateMITM(p, one)
+	simulateMITM(p, p)
+	simulateMITM(p, new(big.Int).Sub(p, one))
 }
