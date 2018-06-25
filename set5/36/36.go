c2b486a125accfd6ef8554f3273a83f9b275c7bf
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-06-24 20:01:30 -0400

    36

--- set5/36/36.go
+++ set5/36/36.go
@@ -1,92 +1,46 @@
 package main
 
 import (
-	"crypto"
-	"crypto/rand"
-	"crypto/sha256"
 	"math/big"
+	"net"
+	"sync"
 )
 
-const (
-	defaultPrime = `ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024
-e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd
-3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec
-6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f
-24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361
-c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552
-bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff
-fffffffffffff`
-	defaultGenerator = `2`
-)
-
-// DHPrivateKey represents a set of Diffie-Hellman parameters and key pair.
-type DHPrivateKey struct {
-	p   *big.Int
-	g   *big.Int
-	n   *big.Int
-	pub crypto.PublicKey
+type record struct {
+	v *big.Int
+	salt []byte
 }
 
-// DHGenerateKey generates a private key.
-func DHGenerateKey(p, g *big.Int) *DHPrivateKey {
-	n, err := rand.Int(rand.Reader, p)
-	if err != nil {
-		panic(err)
-	}
-	pub := crypto.PublicKey(new(big.Int).Exp(g, n, p))
-	return &DHPrivateKey{p, g, n, pub}
+type database map[string]record
+
+type SRPConfig struct {
+	p *big.Int
+	g *big.Int
+	db database
 }
 
-// Public returns the public key.
-func (priv *DHPrivateKey) Public() crypto.PublicKey {
-	return priv.pub
+type SRPConn struct {
+	net.Conn
+	*sync.Mutex
+	isClient bool
 }
 
-// Secret takes a public key and returns a shared secret.
-func (priv *DHPrivateKey) Secret(pub crypto.PublicKey) []byte {
-	return new(big.Int).Exp(pub.(*big.Int), priv.n, priv.p).Bytes()
+func (conn *SRPConn) handshake() error {
 }
 
-// RandomBytes returns a random buffer of the desired length.
-func RandomBytes(n int) []byte {
-	res := make([]byte, n)
-	if _, err := rand.Read(res); err != nil {
-		panic(err)
-	}
-	return res
+func (conn *SRPConn) clientHandshake() error {
 }
 
-// SRPServer represents an SRP server.
-type SRPServer struct {
-	*DHPrivateKey
-	email string
-	v     *big.Int
-	salt  []byte
+func (conn *SRPConn) serverHandshake() error {
 }
 
-// NewSRPServer returns an SRP server.
-func NewSRPServer(p, g *big.Int, email, password string) *SRPServer {
-	salt := RandomBytes(8)
-
-	h := sha256.New()
-	h.Write(salt)
-	h.Write([]byte(password))
-	x := new(big.Int).SetBytes(h.Sum([]byte{}))
-	v := new(big.Int).Exp(g, x, p)
-
-	return &SRPServer{DHGenerateKey(p, g), email, v, salt}
+func (conn *SRPConn) Read(buf []byte) (int, error) {
 }
 
-// SRPClient represents an SRP client.
-type SRPClient struct {
-	*DHPrivateKey
-	email    string
-	password string
+func (conn *SRPConn) Write(buf []byte) (int, error) {
 }
 
-// NewSRPClient returns an SRP client.
-func NewSRPClient(p, g *big.Int, email, password string) *SRPClient {
-	return &SRPClient{DHGenerateKey(p, g), email, password}
+func (conn *SRPConn) Close() error {
 }
 
 func main() {
