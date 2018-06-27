b295fb47f249ce65d205cc636e8ba5e728f52671
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-06-27 19:38:43 -0400

    38

---
+++ set5/38/38_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bytes"
+	"math/big"
+	weak "math/rand"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestSecret(t *testing.T) {
+	p, ok := new(big.Int).SetString(strings.Replace(defaultPrime, "\n", "", -1), 16)
+	if !ok || !p.ProbablyPrime(0) {
+		panic("invalid prime")
+	}
+	g, ok := new(big.Int).SetString(defaultGenerator, 16)
+	if !ok {
+		panic("invalid generator")
+	}
+	a, b := DHGenerateKey(p, g), DHGenerateKey(p, g)
+
+	s1 := a.Secret(b.Public())
+	s2 := b.Secret(a.Public())
+
+	if !bytes.Equal(s1, s2) {
+		t.Errorf(`Secrets not equal:
+p = %x
+g = %x
+a = %x
+A = %x
+b = %x
+B = %x
+(B^a)%%p = %x
+(A^b)%%p = %x`,
+			p, g, a.n, a.Public(), b.n, b.Public(), s1, s2)
+	}
+}
+
+func TestRandomBytes(t *testing.T) {
+	weak := weak.New(weak.NewSource(time.Now().UnixNano()))
+	n := weak.Intn(1024)
+
+	var cases [][]byte
+	for i := 0; i < 5; i++ {
+		buf := RandomBytes(n)
+		if len(buf) != n {
+			t.Errorf("RandomBytes(%v) == %v, length %v",
+				n, buf, len(buf))
+		}
+		cases = append(cases, buf)
+		for j := 0; j < i; j++ {
+			if bytes.Equal(cases[i], cases[j]) {
+				t.Errorf("RandomBytes created identical buffers %v and %v",
+					cases[i], cases[j])
+			}
+		}
+	}
+}
