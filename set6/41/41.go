1bcba68c47a81daf786edea25273a1e7f4783416
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-07-06 14:36:06 -0400

    41

--- set6/41/41.go
+++ set6/41/41.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"crypto/rand"
 	"crypto/sha256"
 	"errors"
 	"fmt"
+	"io"
 	"math/big"
+	"os"
 	"sync"
 	"time"
 )
 
 const defaultExponent = 65537
 
-var one = big.NewInt(1)
+var (
+	one = big.NewInt(1)
+	two = big.NewInt(2)
+)
 
 var errDuplicateMessage = errors.New("duplicate message")
 
@@ -90,63 +96,110 @@ func RSADecrypt(priv *RSAPrivateKey, buf []byte) ([]byte, error) {
 	return z.Bytes(), nil
 }
 
-// unpaddedRSAOracle takes an RSA private key returns an unpadded message recovery oracle.
+// unpaddedRSAOracle takes an RSA private key and returns an unpadded message recovery oracle.
 func unpaddedRSAOracle(priv *RSAPrivateKey) func([]byte) ([]byte, error) {
 	var cache sync.Map
-	return func(buf []byte) ([]byte, error) {
-		sum := fmt.Sprintf("%x", sha256.Sum256(buf))
-		if _, ok := cache.Load(sum); ok {
+	return func(ciphertext []byte) ([]byte, error) {
+		checksum := fmt.Sprintf("%x", sha256.Sum256(ciphertext))
+		if _, ok := cache.Load(checksum); ok {
 			return nil, errDuplicateMessage
 		}
-		cache.Store(sum, time.Now().Unix())
-		res, err := RSADecrypt(priv, buf)
+		cache.Store(checksum, time.Now().Unix())
+		plaintext, err := RSADecrypt(priv, ciphertext)
 		if err != nil {
 			return nil, err
 		}
-		return res, nil
+		return plaintext, nil
 	}
 }
 
+// unpaddedRSABreaker contains data necessary to attack the unpadded RSA oracle.
 type unpaddedRSABreaker struct {
 	*RSAPublicKey
 	oracle func([]byte) ([]byte, error)
 }
 
+// newUnpaddedRSABreaker takes a public key and unpadded RSA oracle, and returns a breaker.
 func newUnpaddedRSABreaker(pub *RSAPublicKey, oracle func([]byte) ([]byte, error)) *unpaddedRSABreaker {
 	return &unpaddedRSABreaker{pub, oracle}
 }
 
-func (x *unpaddedRSABreaker) breakOracle(s string) (string, error) {
-	ciphertext, err := RSAEncrypt(x.RSAPublicKey, []byte(s))
-	if err != nil {
-		return "", err
-	}
-/*
-	if plaintext, err := x.oracle(ciphertext); err != nil {
-		if err != nil {
-			return nil, err
-		} else {
-			return nil, errors.New("breakOracle: not a duplicate message")
-		}
+// breakOracle breaks the unpadded RSA oracle and returns the plaintext.
+func (x *unpaddedRSABreaker) breakOracle(ciphertext []byte) ([]byte, error) {
+	if _, err := x.oracle(ciphertext); err != nil {
+		return nil, err
 	}
 	if _, err := x.oracle(ciphertext); err != errDuplicateMessage {
 		if err != nil {
 			return nil, err
-		} else {
-			return nil, errors.New("breakOracle: not a duplicate message")
 		}
+		return nil, errors.New("breakOracle: not a duplicate message")
 	}
+	z := new(big.Int).Div(x.n, two)
 	c := new(big.Int).SetBytes(ciphertext)
-	s := new(big.Int).Div(x.n)
-	cPrime := new(big.Int).Exp(s, x.e, x.n)
+	cPrime := new(big.Int).Exp(z, x.e, x.n)
+	cPrime.Mul(cPrime, c)
+	cPrime.Mod(cPrime, x.n)
+
 	plaintextPrime, err := x.oracle(cPrime.Bytes())
+	if err != nil {
+		return nil, err
+	}
+	zInv := z.ModInverse(z, x.n)
 	pPrime := new(big.Int).SetBytes(plaintextPrime)
-	sInv := new(big.Int).ModInverse(s, x.n)
-	p := new(big.Int).Mul(pPrime, sInv)
+	pPrime.Mul(pPrime, zInv)
+	p := pPrime.Mod(pPrime, x.n)
 
 	return p.Bytes(), nil
-*/
+}
+
+// printUnpaddedRSA reads lines of text, encrypts them, and prints the decrypted plaintext.
+func printUnpaddedRSA(in io.Reader, x *unpaddedRSABreaker) error {
+	input := bufio.NewScanner(in)
+	for input.Scan() {
+		ciphertext, err := RSAEncrypt(x.RSAPublicKey, input.Bytes())
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		plaintext, err := x.breakOracle(ciphertext)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		fmt.Printf("ciphertext: %x\nplaintext: %s\n",
+			ciphertext, plaintext)
+	}
+	return input.Err()
 }
 
 func main() {
+	fmt.Print("generating RSA key...")
+	priv, err := RSAGenerateKey(defaultExponent, 1024)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("done.")
+	oracle := unpaddedRSAOracle(priv)
+	x := newUnpaddedRSABreaker(&priv.RSAPublicKey, oracle)
+
+	files := os.Args[1:]
+	// If no files are specified, read from standard input.
+	if len(files) == 0 {
+		if err := printUnpaddedRSA(os.Stdin, x); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		return
+	}
+	for _, name := range files {
+		f, err := os.Open(name)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		if err := printUnpaddedRSA(f, x); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		f.Close()
+	}
 }
