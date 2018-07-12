c7f339be73a820fb7e3b19f50d124e3e3c79d1fb
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-07-12 17:10:34 -0400

    1

--- set1/1/1_test.go
+++ set1/1/1_test.go
@@ -11,10 +11,11 @@ import (
 func init() { weak.Seed(time.Now().UnixNano()) }
 
 func TestHexToBase64(t *testing.T) {
-	n := 16 + weak.Intn(16)
-	buf := make([]byte, n)
 	for i := 0; i < 5; i++ {
+		n := 16 + weak.Intn(16)
+		buf := make([]byte, n)
 		weak.Read(buf)
+
 		s := hex.EncodeToString(buf)
 		want := base64.StdEncoding.EncodeToString(buf)
 
