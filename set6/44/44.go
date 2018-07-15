9fdcb852663f76cb4a7202ab9adbc5d671080098
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-07-15 17:55:35 -0400

    44

--- set6/44/44.go
+++ set6/44/44.go
@@ -6,22 +6,22 @@ import (
 	"crypto/sha1"
 	"encoding/hex"
 	"errors"
+	"io"
 	"math/big"
 	"strings"
 )
 
 // message represents a message signed with DSA.
 type message struct {
-	s   *big.Int
-	r   *big.Int
-	sum []byte
+	s *big.Int
+	r *big.Int
+	m *big.Int
 }
 
 // scanAfterPrefix reads a line and returns the string after a prefix.
 func scanAfterPrefix(input *bufio.Scanner, prefix string) (string, error) {
-	input.Scan()
-	if err := input.Err(); err != nil {
-		return "", err
+	if input.Scan(); input.Err() != nil {
+		return "", input.Err()
 	}
 	if !strings.HasPrefix(input.Text(), prefix) {
 		return "", errors.New("scanAfterPrefix: invalid input")
@@ -68,12 +68,44 @@ func scanMessage(input *bufio.Scanner) (*message, error) {
 	if s, err = scanAfterPrefix(input, "m: "); err != nil {
 		return nil, err
 	}
-	if msg.sum, err = hex.DecodeString(s); err != nil {
+	if sum, err := hex.DecodeString(s); err != nil {
 		return nil, err
-	} else if !bytes.Equal(msg.sum, array[:]) {
+	} else if !bytes.Equal(sum, array[:]) {
 		return nil, errors.New("scanMessage: invalid checksum")
+	} else {
+		msg.m = new(big.Int).SetBytes(sum)
 	}
 	return msg, nil
+}
+
+// readMessages reads all messages from the input.
+func readMessages(in io.Reader) ([]*message, error) {
+	input := bufio.NewScanner(in)
+	var msgs []*message
+	for {
+		if msg, err := scanMessage(input); err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		} else {
+			msgs = append(msgs, msg)
+		}
+	}
+	return msgs, input.Err()
+}
+
+// generatePairs returns a channel that yields all pairs of messages.
+func generatePairs(msgs []*message) <-chan []*message {
+	ch := make(chan []*message)
+	go func() {
+		for i := 0; i < len(msgs)-1; i++ {
+			for j := i + 1; j < len(msgs); j++ {
+				ch <- append([]*message{msgs[i]}, msgs[j])
+			}
+		}
+		close(ch)
+	}()
+	return ch
 }
 
 func main() {
