338344ad77ae6bb8cbd85e9e57997360f3ee5a8b
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-07-25 08:26:33 -0400

    47

--- set6/47/47.go
+++ set6/47/47.go
@@ -6,11 +6,6 @@ import (
 	"math/big"
 )
 
-const (
-	rsaDefaultE    = 65537
-	rsaDefaultBits = 2048
-)
-
 var one = big.NewInt(1)
 
 // RSAPublicKey represents the public part of an RSA key pair.
@@ -83,7 +78,7 @@ func copyRight(dst, src []byte) {
 	copy(dst, src)
 }
 
-// RSAEncrypt takes an encrypted buffer and returns a decrypted buffer.
+// RSAEncrypt takes a public key and plaintext, and returns ciphertext.
 func RSAEncrypt(pub *RSAPublicKey, buf []byte) ([]byte, error) {
 	z := new(big.Int).SetBytes(buf)
 	if z.Cmp(pub.n) > 0 {
@@ -97,7 +92,7 @@ func RSAEncrypt(pub *RSAPublicKey, buf []byte) ([]byte, error) {
 	return res, nil
 }
 
-// RSADecrypt takes a decrypted buffer and returns an encrypted buffer.
+// RSADecrypt takes a private key and ciphertext, and returns plaintext.
 func RSADecrypt(priv *RSAPrivateKey, buf []byte) ([]byte, error) {
 	z := new(big.Int).SetBytes(buf)
 	if z.Cmp(priv.n) > 0 {
@@ -109,6 +104,79 @@ func RSADecrypt(priv *RSAPrivateKey, buf []byte) ([]byte, error) {
 	copyRight(res, z.Bytes())
 
 	return res, nil
+}
+
+// RandomBytes returns a random buffer of the desired length.
+func RandomBytes(n int) []byte {
+	buf := make([]byte, n)
+	if _, err := rand.Read(buf); err != nil {
+		panic(err)
+	}
+	return buf
+}
+
+// RandomNonzeroBytes returns a random buffer of the desired length containing no zero bytes.
+func RandomNonzeroBytes(n int) []byte {
+	buf := RandomBytes(n)
+	for i, b := range buf {
+		if b == 0 {
+			buf[i]++
+		}
+	}
+	return buf
+}
+
+// PKCS1v15CryptPad returns a checksum with PKCS #1 v1.5 signature padding added.
+func PKCS1v15CryptPad(buf []byte, size int) ([]byte, error) {
+	if len(buf)+11 > size {
+		return nil, errors.New("PKCS1v15CryptPad: buffer too large")
+	}
+	n := size - len(buf) - 3
+
+	buf = append([]byte{0x00}, buf...)
+	buf = append(RandomNonzeroBytes(n), buf...)
+	buf = append([]byte{0x00, 0x02}, buf...)
+
+	return buf, nil
+}
+
+// PKCS1v15CryptUnpad returns a checksum with PKCS #1 v1.5 signature padding removed.
+func PKCS1v15CryptUnpad(buf []byte) ([]byte, error) {
+	errInvalidPadding := errors.New("PKCS1v15CryptUnpad: invalid padding")
+	if buf[0] != 0x00 {
+		return nil, errInvalidPadding
+	}
+	buf = buf[1:]
+	if buf[0] != 0x02 {
+		return nil, errInvalidPadding
+	}
+	for len(buf) > 0 && buf[0] != 0x00 {
+		buf = buf[1:]
+	}
+	if len(buf) == 0 {
+		return nil, errInvalidPadding
+	}
+	buf = buf[1:]
+
+	return buf, nil
+}
+
+// RSAEncryptPKCS1v15 takes a public key and plaintext, and returns PKCS #1 v1.5 padded ciphertext.
+func RSAEncryptPKCS1v15(pub *RSAPublicKey, buf []byte) ([]byte, error) {
+	buf, err := PKCS1v15CryptPad(buf, size(pub.n))
+	if err != nil {
+		return nil, err
+	}
+	return RSAEncrypt(pub, buf)
+}
+
+// RSADecryptPKCS1v15 takes a private key and PKCS #1 v1.5 padded ciphertext, and returns plaintext.
+func RSADecryptPKCS1v15(priv *RSAPrivateKey, buf []byte) ([]byte, error) {
+	buf, err := RSADecrypt(priv, buf)
+	if err != nil {
+		return nil, err
+	}
+	return PKCS1v15CryptUnpad(buf)
 }
 
 func main() {
--- set6/47/47_test.go
+++ set6/47/47_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"math/big"
 	weak "math/rand"
 	"testing"
@@ -29,6 +30,60 @@ func TestRSA(t *testing.T) {
 			t.Error(err)
 		}
 		plaintext, err := RSADecrypt(priv, ciphertext)
+		if err != nil {
+			t.Error(err)
+		}
+		want := new(big.Int).SetBytes(buf)
+		got := new(big.Int).SetBytes(plaintext)
+		if !equal(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+}
+
+func TestPKCS1v15CryptPadUnpad(t *testing.T) {
+	priv, err := RSAGenerateKey(3, 256)
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < 5; i++ {
+		want := make([]byte, weak.Intn(size(priv.n)-10))
+		weak.Read(want)
+
+		buf, err := PKCS1v15CryptPad(want, size(priv.n))
+		if err != nil {
+			t.Fatal(err)
+		}
+		got, err := PKCS1v15CryptUnpad(buf)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !bytes.Equal(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+}
+
+func TestRSACryptPKCS1v15(t *testing.T) {
+	const (
+		exponent = 3
+		bits     = 256
+	)
+	buf := make([]byte, 16)
+	for i := 0; i < 5; i++ {
+		priv, err := RSAGenerateKey(exponent, bits)
+		if err != nil {
+			t.Error(err)
+		}
+		if n := priv.n.BitLen(); n != bits {
+			t.Errorf("got bit size %v, want %v", n, bits)
+		}
+		weak.Read(buf)
+		ciphertext, err := RSAEncryptPKCS1v15(priv.Public(), buf)
+		if err != nil {
+			t.Error(err)
+		}
+		plaintext, err := RSADecryptPKCS1v15(priv, ciphertext)
 		if err != nil {
 			t.Error(err)
 		}
