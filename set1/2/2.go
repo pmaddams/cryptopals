8d5eb0c17fd178f3e9107f84e4bd607c9c4bf540
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-07-26 12:02:36 -0400

    2

--- set1/2/2.go
+++ set1/2/2.go
@@ -24,31 +24,25 @@ func XORBytes(dst, b1, b2 []byte) int {
 	return n
 }
 
-// xorTwoLines reads two hex-encoded lines and prints their XOR combination.
-func xorTwoLines(in io.Reader) error {
-	var s1, s2 string
-	if _, err := fmt.Fscanln(in, &s1); err != nil {
-		return err
-	} else if _, err = fmt.Fscanln(in, &s2); err != nil {
+// xorLines reads two hex-encoded lines and prints their XOR combination.
+func xorLines(in io.Reader) error {
+	var s string
+	if _, err := fmt.Fscanln(in, &s); err != nil {
 		return err
 	}
-	b1, err := hex.DecodeString(s1)
+	b1, err := hex.DecodeString(s)
 	if err != nil {
 		return err
 	}
-	b2, err := hex.DecodeString(s2)
+	if _, err := fmt.Fscanln(in, &s); err != nil {
+		return err
+	}
+	b2, err := hex.DecodeString(s)
 	if err != nil {
 		return err
 	}
-	// Write the data in place to the shorter buffer.
-	var dst []byte
-	if len(b1) < len(b2) {
-		dst = b1
-	} else {
-		dst = b2
-	}
-	XORBytes(dst, b1, b2)
-	fmt.Printf("%x\n", dst)
+	n := XORBytes(b1, b1, b2)
+	fmt.Printf("%x\n", b1[:n])
 
 	return nil
 }
@@ -57,7 +51,7 @@ func main() {
 	files := os.Args[1:]
 	// If no files are specified, read from standard input.
 	if len(files) == 0 {
-		if err := xorTwoLines(os.Stdin); err != nil {
+		if err := xorLines(os.Stdin); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
 		return
@@ -68,7 +62,7 @@ func main() {
 			fmt.Fprintln(os.Stderr, err)
 			continue
 		}
-		if err := xorTwoLines(f); err != nil {
+		if err := xorLines(f); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
 		f.Close()
