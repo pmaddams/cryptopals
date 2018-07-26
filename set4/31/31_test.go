f7533553253a13cfdc9e7fece47f95eb6e466c74
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-07-26 12:08:34 -0400

    tests

--- set4/31/31_test.go
+++ set4/31/31_test.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	reference "crypto/hmac"
 	"crypto/sha1"
-	"encoding/hex"
 	weak "math/rand"
 	"testing"
 	"time"
@@ -13,34 +12,30 @@ import (
 func init() { weak.Seed(time.Now().UnixNano()) }
 
 func TestXORBytes(t *testing.T) {
-	decodeString := func(s string) []byte {
-		buf, _ := hex.DecodeString(s)
-		return buf
-	}
 	cases := []struct {
 		b1, b2, want []byte
 	}{
 		{
-			decodeString("1c0111001f010100061a024b53535009181c"),
-			decodeString("686974207468652062756c6c277320657965"),
-			decodeString("746865206b696420646f6e277420706c6179"),
-		},
-		{
 			[]byte{0, 0, 0, 0},
 			[]byte{1, 1, 1, 1},
 			[]byte{1, 1, 1, 1},
 		},
 		{
 			[]byte{1, 0, 1, 0},
-			[]byte{1, 0, 1, 0},
+			[]byte{1, 0, 1, 0, 1, 0},
 			[]byte{0, 0, 0, 0},
+		},
+		{
+			[]byte{1, 0, 1, 0, 1, 0},
+			[]byte{1, 1, 1, 1},
+			[]byte{0, 1, 0, 1},
 		},
 	}
 	for _, c := range cases {
-		dst := make([]byte, len(c.b1))
-		XORBytes(dst, c.b1, c.b2)
-		if !bytes.Equal(dst, c.want) {
-			t.Errorf("got %v, want %v", dst, c.want)
+		n := XORBytes(c.b1, c.b1, c.b2)
+		got := c.b1[:n]
+		if !bytes.Equal(got, c.want) {
+			t.Errorf("got %v, want %v", got, c.want)
 		}
 	}
 }
