fda87f3944f388cd2d18d4a8026562c580f41e0e
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-04-22 16:57:15 -0400

    5

--- set1/05/main.go
+++ set1/05/main.go
@@ -15,6 +15,72 @@ import (
 
 const secret = "ICE"
 
+func main() {
+	var (
+		d  bool
+		fn func(io.Reader, cipher.Stream) error
+	)
+	flag.BoolVar(&d, "d", false, "decrypt")
+	flag.Parse()
+	if d {
+		fn = decrypt
+	} else {
+		fn = encrypt
+	}
+	files := flag.Args()
+	if len(files) == 0 {
+		stream := NewXORCipher([]byte(secret))
+		if err := fn(os.Stdin, stream); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+	for _, file := range files {
+		f, err := os.Open(file)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		stream := NewXORCipher([]byte(secret))
+		if err := fn(f, stream); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		f.Close()
+	}
+}
+
+// encrypt reads plaintext and prints hex-encoded ciphertext.
+func encrypt(in io.Reader, stream cipher.Stream) error {
+	buf, err := ioutil.ReadAll(in)
+	if err != nil {
+		return err
+	}
+	stream.XORKeyStream(buf, buf)
+	fmt.Println(hex.EncodeToString(buf))
+
+	return nil
+}
+
+// decrypt reads hex-encoded ciphertext and prints plaintext.
+func decrypt(in io.Reader, stream cipher.Stream) error {
+	var buf []byte
+
+	input := bufio.NewScanner(in)
+	for input.Scan() {
+		line, err := hex.DecodeString(input.Text())
+		if err != nil {
+			return err
+		}
+		buf = append(buf, line...)
+	}
+	if err := input.Err(); err != nil {
+		return err
+	}
+	stream.XORKeyStream(buf, buf)
+	fmt.Print(string(buf))
+
+	return nil
+}
+
 // xorCipher represents a repeating XOR stream cipher.
 type xorCipher struct {
 	key []byte
@@ -35,72 +101,5 @@ func (x *xorCipher) XORKeyStream(dst, src []byte) {
 		if x.pos == len(x.key) {
 			x.pos = 0
 		}
-	}
-}
-
-// encryptXOR reads plaintext and prints hex-encoded ciphertext.
-func encryptXOR(in io.Reader, stream cipher.Stream) error {
-	buf, err := ioutil.ReadAll(in)
-	if err != nil {
-		return err
-	}
-	stream.XORKeyStream(buf, buf)
-	fmt.Println(hex.EncodeToString(buf))
-
-	return nil
-}
-
-// decryptXOR reads hex-encoded ciphertext and prints plaintext.
-func decryptXOR(in io.Reader, stream cipher.Stream) error {
-	input := bufio.NewScanner(in)
-	var buf []byte
-	for input.Scan() {
-		line, err := hex.DecodeString(input.Text())
-		if err != nil {
-			return err
-		}
-		buf = append(buf, line...)
-	}
-	if err := input.Err(); err != nil {
-		return err
-	}
-	stream.XORKeyStream(buf, buf)
-	fmt.Print(string(buf))
-
-	return nil
-}
-
-func main() {
-	var (
-		d  bool
-		fn func(io.Reader, cipher.Stream) error
-	)
-	flag.BoolVar(&d, "d", false, "decrypt")
-	flag.Parse()
-	if d {
-		fn = decryptXOR
-	} else {
-		fn = encryptXOR
-	}
-	files := flag.Args()
-	// If no files are specified, read from standard input.
-	if len(files) == 0 {
-		stream := NewXORCipher([]byte(secret))
-		if err := fn(os.Stdin, stream); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	}
-	for _, file := range files {
-		f, err := os.Open(file)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			continue
-		}
-		// Since the stream is stateful, we have to re-initialize it.
-		stream := NewXORCipher([]byte(secret))
-		if err := fn(f, stream); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-		f.Close()
 	}
 }
