add26ae21db6e7a233a6039ab113e328483a073a
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-04-22 18:06:38 -0400

    6

--- set1/06/main.go
+++ set1/06/main.go
@@ -17,8 +17,121 @@ import (
 // sample is a file similar to the expected plaintext.
 const sample = "alice.txt"
 
-// EditDistance returns the number of differing bits between two buffers.
-func EditDistance(b1, b2 []byte) int {
+func main() {
+	f, err := os.Open(sample)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	score, err := ScoreFunc(f)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	f.Close()
+
+	files := os.Args[1:]
+	if len(files) == 0 {
+		if err := decrypt(os.Stdin, score); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+	for _, file := range files {
+		f, err := os.Open(file)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		if err := decrypt(f, score); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		f.Close()
+	}
+}
+
+// decrypt reads base64-encoded ciphertext and prints plaintext.
+func decrypt(in io.Reader, score func([]byte) int) error {
+	buf, err := ioutil.ReadAll(base64.NewDecoder(base64.StdEncoding, in))
+	if err != nil {
+		return err
+	}
+	key, err := breakRepeatingXOR(buf, score)
+	if err != nil {
+		return err
+	}
+	stream := NewXORCipher(key)
+	stream.XORKeyStream(buf, buf)
+	fmt.Print(string(buf))
+
+	return nil
+}
+
+// breakRepeatingXOR returns the key used to encrypt a buffer with repeating XOR.
+func breakRepeatingXOR(buf []byte, score func([]byte) int) ([]byte, error) {
+	size, err := keySize(buf)
+	if err != nil {
+		return nil, err
+	}
+	bufs, err := Transpose(Subdivide(buf, size))
+	if err != nil {
+		return nil, err
+	}
+	key := make([]byte, size)
+
+	var wg sync.WaitGroup
+	for i := range bufs {
+		wg.Add(1)
+		// Capture the value of the loop variable.
+		go func(i int) {
+			key[i] = breakSingleXOR(bufs[i], score)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+
+	return key, nil
+}
+
+// keySize returns the probable key size of a buffer encrypted with repeating XOR.
+func keySize(buf []byte) (int, error) {
+	// Guess lower and upper bounds.
+	const (
+		lower = 2
+		upper = 64
+	)
+	var n int
+	best := float64(8 * len(buf))
+	for size := lower; size <= upper; size++ {
+		// If the block size is too large, stop.
+		if distance, err := AverageDistance(buf, size); err != nil {
+			if n < lower {
+				return 0, errors.New("keySize: nothing found")
+			}
+			break
+		} else if distance < best {
+			best = distance
+			n = size
+		}
+	}
+	return n, nil
+}
+
+// AverageDistance returns the average Hamming distance between adjacent blocks.
+func AverageDistance(buf []byte, size int) (float64, error) {
+	blocks := Subdivide(buf, size)
+	if len(blocks) < 2 {
+		return 0, errors.New("AverageDistance: need 2 or more blocks")
+	}
+	var f float64
+	for i := 0; i < len(blocks)-1; i++ {
+		n := HammingDistance(blocks[i], blocks[i+1])
+		f += float64(n) / float64(size) / float64(len(blocks)-1)
+	}
+	return f, nil
+}
+
+// HammingDistance returns the number of differing bits between two buffers.
+func HammingDistance(b1, b2 []byte) int {
 	var short, long []byte
 	if len(b1) < len(b2) {
 		short, long = b1, b2
@@ -34,71 +147,54 @@ func EditDistance(b1, b2 []byte) int {
 	return n
 }
 
-// Blocks divides a buffer into blocks.
-func Blocks(buf []byte, blockSize int) [][]byte {
-	var bufs [][]byte
-	for len(buf) >= blockSize {
-		// Return pointers, not copies.
-		bufs = append(bufs, buf[:blockSize])
-		buf = buf[blockSize:]
-	}
-	return bufs
-}
-
-// NormalizedDistance returns the normalized edit distance between pairs of blocks.
-func NormalizedDistance(buf []byte, blockSize int) (float64, error) {
-	bufs := Blocks(buf, blockSize)
-	if len(bufs) < 2 {
-		return 0, errors.New("NormalizedDistance: need 2 or more blocks")
-	}
-	var f float64
-	for i := 0; i < len(bufs)-1; i++ {
-		n := EditDistance(bufs[i], bufs[i+1])
-		f += float64(n) / float64(blockSize) / float64(len(bufs)-1)
-	}
-	return f, nil
-}
-
-// keySize returns the probable key size of a buffer encrypted with repeating XOR.
-func keySize(buf []byte) (int, error) {
-	// Guess lower and upper bounds.
-	const (
-		lower = 2
-		upper = 64
-	)
-	var n int
-	best := float64(8 * len(buf))
-	for blockSize := lower; blockSize <= upper; blockSize++ {
-		// If the block size is too large, stop.
-		if distance, err := NormalizedDistance(buf, blockSize); err != nil {
-			if n < lower {
-				return 0, errors.New("keySize: nothing found")
-			}
-			break
-		} else if distance < best {
-			best = distance
-			n = blockSize
+// Transpose takes a slice of blocks and returns buffers swapping the rows and columns.
+func Transpose(blocks [][]byte) ([][]byte, error) {
+	for i := 0; i < len(blocks); i++ {
+		if len(blocks[i]) != len(blocks[0]) {
+			return nil, errors.New("Transpose: blocks must have equal length")
 		}
 	}
-	return n, nil
+	bufs := make([][]byte, len(blocks[0]))
+	for i := 0; i < len(blocks[0]); i++ {
+		bufs[i] = make([]byte, len(blocks))
+		for j := 0; j < len(blocks); j++ {
+			bufs[i][j] = blocks[j][i]
+		}
+	}
+	return bufs, nil
 }
 
-// Symbols reads sample text and returns a map of UTF-8 symbol counts.
-func Symbols(in io.Reader) (map[rune]int, error) {
-	buf, err := ioutil.ReadAll(in)
-	if err != nil {
-		return nil, err
+// Subdivide divides a buffer into blocks.
+func Subdivide(buf []byte, size int) [][]byte {
+	var blocks [][]byte
+	for len(buf) >= size {
+		// Return pointers, not copies.
+		blocks = append(blocks, buf[:size])
+		buf = buf[size:]
 	}
-	m := make(map[rune]int)
-	for _, r := range string(buf) {
-		m[r]++
-	}
-	return m, nil
+	return blocks
 }
 
-// Score reads sample text and returns a scoring function.
-func Score(in io.Reader) (func([]byte) int, error) {
-	m, err := Symbols(in)
+// breakSingleXOR returns the key used to encrypt a buffer with single-byte XOR.
+func breakSingleXOR(buf []byte, score func([]byte) int) byte {
+	var (
+		key  byte
+		best int
+	)
+	tmp := make([]byte, len(buf))
+	for i := 0; i <= 0xff; i++ {
+		XORSingleByte(tmp, buf, byte(i))
+		if n := score(tmp); n > best {
+			best = n
+			key = byte(i)
+		}
+	}
+	return key
+}
+
+// ScoreFunc reads sample text and returns a scoring function.
+func ScoreFunc(in io.Reader) (func([]byte) int, error) {
+	m, err := SymbolCounts(in)
 	if err != nil {
 		return nil, err
 	}
@@ -111,88 +207,25 @@ func Score(in io.Reader) (func([]byte) int, error) {
 	}, nil
 }
 
+// SymbolCounts reads sample text and returns a map of UTF-8 symbol counts.
+func SymbolCounts(in io.Reader) (map[rune]int, error) {
+	buf, err := ioutil.ReadAll(in)
+	if err != nil {
+		return nil, err
+	}
+	m := make(map[rune]int)
+	for _, r := range string(buf) {
+		m[r]++
+	}
+	return m, nil
+}
+
 // XORSingleByte produces the XOR combination of a buffer with a single byte.
 func XORSingleByte(dst, src []byte, b byte) {
 	// Panic if dst is smaller than src.
 	for i := range src {
 		dst[i] = src[i] ^ b
 	}
-}
-
-// breakSingleXOR takes a buffer and score function, and returns the single-byte XOR key.
-func breakSingleXOR(buf []byte, score func([]byte) int) byte {
-	// Don't modify the original data.
-	tmp := make([]byte, len(buf))
-	var (
-		key  byte
-		best int
-	)
-	// Use an integer as the loop variable to avoid overflow.
-	for i := 0; i <= 0xff; i++ {
-		XORSingleByte(tmp, buf, byte(i))
-		if n := score(tmp); n > best {
-			best = n
-			key = byte(i)
-		}
-	}
-	return key
-}
-
-// Lengths returns a slice of integer buffer lengths.
-func Lengths(bufs [][]byte) []int {
-	var nums []int
-	for _, buf := range bufs {
-		nums = append(nums, len(buf))
-	}
-	return nums
-}
-
-// Transpose takes equal-length buffers and returns them with the rows and columns swapped.
-func Transpose(bufs [][]byte) ([][]byte, error) {
-	nums := Lengths(bufs)
-	if len(nums) == 0 {
-		return nil, errors.New("Transpose: no data")
-	}
-	n := nums[0]
-	for i := range nums[1:] {
-		if nums[i] != n {
-			return nil, errors.New("Transpose: buffers must have equal lengths")
-		}
-	}
-	res := make([][]byte, n)
-	for i := range res {
-		res[i] = make([]byte, len(bufs))
-		for j := range res[i] {
-			res[i][j] = bufs[j][i]
-		}
-	}
-	return res, nil
-}
-
-// breakXOR returns the key used to encrypt a buffer with repeating XOR.
-func breakXOR(buf []byte, score func([]byte) int) ([]byte, error) {
-	blockSize, err := keySize(buf)
-	if err != nil {
-		return nil, err
-	}
-	bufs, err := Transpose(Blocks(buf, blockSize))
-	if err != nil {
-		return nil, err
-	}
-	key := make([]byte, blockSize)
-
-	var wg sync.WaitGroup
-	for i := range bufs {
-		wg.Add(1)
-		// Capture the value of the loop variable.
-		go func(i int) {
-			key[i] = breakSingleXOR(bufs[i], score)
-			wg.Done()
-		}(i)
-	}
-	wg.Wait()
-
-	return key, nil
 }
 
 // xorCipher represents a repeating XOR stream cipher.
@@ -215,55 +248,5 @@ func (x *xorCipher) XORKeyStream(dst, src []byte) {
 		if x.pos == len(x.key) {
 			x.pos = 0
 		}
-	}
-}
-
-// decryptXOR reads base64-encoded ciphertext and prints plaintext.
-func decryptXOR(in io.Reader, score func([]byte) int) error {
-	buf, err := ioutil.ReadAll(base64.NewDecoder(base64.StdEncoding, in))
-	if err != nil {
-		return err
-	}
-	key, err := breakXOR(buf, score)
-	if err != nil {
-		return err
-	}
-	stream := NewXORCipher(key)
-	stream.XORKeyStream(buf, buf)
-	fmt.Print(string(buf))
-
-	return nil
-}
-
-func main() {
-	f, err := os.Open(sample)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return
-	}
-	score, err := Score(f)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return
-	}
-	f.Close()
-
-	files := os.Args[1:]
-	// If no files are specified, read from standard input.
-	if len(files) == 0 {
-		if err := decryptXOR(os.Stdin, score); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	}
-	for _, file := range files {
-		f, err := os.Open(file)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			continue
-		}
-		if err := decryptXOR(f, score); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-		f.Close()
 	}
 }
--- set1/06/main_test.go
+++ set1/06/main_test.go
@@ -8,35 +8,7 @@ import (
 	"testing"
 )
 
-func TestEditDistance(t *testing.T) {
-	cases := []struct {
-		b1, b2 []byte
-		want   int
-	}{
-		{
-			[]byte("this is a test"),
-			[]byte("wokka wokka!!!"),
-			37,
-		},
-		{
-			[]byte{0, 0, 0, 0},
-			[]byte{1, 2, 4, 8},
-			1 + 1 + 1 + 1,
-		},
-		{
-			[]byte{1, 2, 3, 4, 5},
-			[]byte{6, 7, 8, 9},
-			3 + 2 + 3 + 3 + 8,
-		},
-	}
-	for _, c := range cases {
-		if got := EditDistance(c.b1, c.b2); got != c.want {
-			t.Errorf("got %v, want %v", got, c.want)
-		}
-	}
-}
-
-func TestNormalizedDistance(t *testing.T) {
+func TestAverageDistance(t *testing.T) {
 	cases := []struct {
 		buf       []byte
 		blockSize int
@@ -59,171 +31,35 @@ func TestNormalizedDistance(t *testing.T) {
 		},
 	}
 	for _, c := range cases {
-		if got, _ := NormalizedDistance(c.buf, c.blockSize); got != c.want {
+		if got, _ := AverageDistance(c.buf, c.blockSize); got != c.want {
 			t.Errorf("got %v, want %v", got, c.want)
 		}
 	}
 }
 
-func TestSymbols(t *testing.T) {
+func TestHammingDistance(t *testing.T) {
 	cases := []struct {
-		s    string
-		want map[rune]int
-	}{
-		{
-			"hello world",
-			map[rune]int{
-				'h': 1,
-				'e': 1,
-				'l': 3,
-				'o': 2,
-				' ': 1,
-				'w': 1,
-				'r': 1,
-				'd': 1,
-			},
-		},
-		{
-			"你好世界",
-			map[rune]int{
-				'你': 1,
-				'好': 1,
-				'世': 1,
-				'界': 1,
-			},
-		},
-	}
-	for _, c := range cases {
-		got, _ := Symbols(strings.NewReader(c.s))
-		if !reflect.DeepEqual(got, c.want) {
-			t.Errorf("got %v, want %v", got, c.want)
-		}
-	}
-}
-
-func TestScore(t *testing.T) {
-	cases := []struct {
-		sample string
-		s      string
+		b1, b2 []byte
 		want   int
 	}{
 		{
-			"hola",
-			"hello world",
-			6,
+			[]byte("this is a test"),
+			[]byte("wokka wokka!!!"),
+			37,
 		},
 		{
-			"世界再见",
-			"你好世界",
-			2,
+			[]byte{0, 0, 0, 0},
+			[]byte{1, 2, 4, 8},
+			1 + 1 + 1 + 1,
+		},
+		{
+			[]byte{1, 2, 3, 4, 5},
+			[]byte{6, 7, 8, 9},
+			3 + 2 + 3 + 3 + 8,
 		},
 	}
 	for _, c := range cases {
-		score, err := Score(strings.NewReader(c.sample))
-		if err != nil {
-			t.Fatal(err)
-		}
-		if got := score([]byte(c.s)); got != c.want {
-			t.Errorf("got %v, want %v", got, c.want)
-		}
-	}
-}
-
-func TestXORSingleByte(t *testing.T) {
-	cases := []struct {
-		src  []byte
-		b    byte
-		want []byte
-	}{
-		{
-			[]byte{0, 1, 2, 3, 4, 5},
-			1,
-			[]byte{1, 0, 3, 2, 5, 4},
-		},
-		{
-			[]byte{0, 1, 2, 3, 4, 5},
-			2,
-			[]byte{2, 3, 0, 1, 6, 7},
-		},
-		{
-			[]byte{0, 1, 2, 3, 4, 5},
-			3,
-			[]byte{3, 2, 1, 0, 7, 6},
-		},
-	}
-	dst := make([]byte, 6)
-	for _, c := range cases {
-		XORSingleByte(dst, c.src, c.b)
-		if !bytes.Equal(dst, c.want) {
-			t.Errorf("got %v, want %v", dst, c.want)
-		}
-	}
-}
-
-func TestLengths(t *testing.T) {
-	cases := []struct {
-		bufs [][]byte
-		want []int
-	}{
-		{
-			[][]byte{},
-			nil,
-		},
-		{
-			[][]byte{
-				{},
-			},
-			[]int{0},
-		},
-		{
-			[][]byte{
-				{1},
-				{1, 2},
-				{1, 2, 3},
-			},
-			[]int{1, 2, 3},
-		},
-	}
-	for _, c := range cases {
-		got := Lengths(c.bufs)
-		if !reflect.DeepEqual(got, c.want) {
-			t.Errorf("got %v, want %v", got, c.want)
-		}
-	}
-}
-
-func TestBlocks(t *testing.T) {
-	cases := []struct {
-		buf  []byte
-		n    int
-		want [][]byte
-	}{
-		{
-			[]byte{1, 2},
-			3,
-			nil,
-		},
-		{
-			[]byte{1, 2, 3, 4, 5, 6},
-			3,
-			[][]byte{
-				{1, 2, 3},
-				{4, 5, 6},
-			},
-		},
-		{
-			[]byte{1, 2, 3, 4, 5, 6},
-			2,
-			[][]byte{
-				{1, 2},
-				{3, 4},
-				{5, 6},
-			},
-		},
-	}
-	for _, c := range cases {
-		got := Blocks(c.buf, c.n)
-		if !reflect.DeepEqual(got, c.want) {
+		if got := HammingDistance(c.b1, c.b2); got != c.want {
 			t.Errorf("got %v, want %v", got, c.want)
 		}
 	}
@@ -231,8 +67,8 @@ func TestBlocks(t *testing.T) {
 
 func TestTranspose(t *testing.T) {
 	cases := []struct {
-		bufs [][]byte
-		want [][]byte
+		blocks [][]byte
+		want   [][]byte
 	}{
 		{
 			[][]byte{
@@ -268,9 +104,141 @@ func TestTranspose(t *testing.T) {
 		},
 	}
 	for _, c := range cases {
-		got, _ := Transpose(c.bufs)
+		got, _ := Transpose(c.blocks)
 		if !reflect.DeepEqual(got, c.want) {
 			t.Errorf("got %v, want %v", got, c.want)
+		}
+	}
+}
+
+func TestSubdivide(t *testing.T) {
+	cases := []struct {
+		buf  []byte
+		n    int
+		want [][]byte
+	}{
+		{
+			[]byte{1, 2},
+			3,
+			nil,
+		},
+		{
+			[]byte{1, 2, 3, 4, 5, 6},
+			3,
+			[][]byte{
+				{1, 2, 3},
+				{4, 5, 6},
+			},
+		},
+		{
+			[]byte{1, 2, 3, 4, 5, 6},
+			2,
+			[][]byte{
+				{1, 2},
+				{3, 4},
+				{5, 6},
+			},
+		},
+	}
+	for _, c := range cases {
+		got := Subdivide(c.buf, c.n)
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("got %v, want %v", got, c.want)
+		}
+	}
+}
+
+func TestScoreFunc(t *testing.T) {
+	cases := []struct {
+		sample string
+		s      string
+		want   int
+	}{
+		{
+			"hola",
+			"hello world",
+			6,
+		},
+		{
+			"世界再见",
+			"你好世界",
+			2,
+		},
+	}
+	for _, c := range cases {
+		score, err := ScoreFunc(strings.NewReader(c.sample))
+		if err != nil {
+			t.Fatal(err)
+		}
+		if got := score([]byte(c.s)); got != c.want {
+			t.Errorf("got %v, want %v", got, c.want)
+		}
+	}
+}
+
+func TestSymbolCounts(t *testing.T) {
+	cases := []struct {
+		s    string
+		want map[rune]int
+	}{
+		{
+			"hello world",
+			map[rune]int{
+				'h': 1,
+				'e': 1,
+				'l': 3,
+				'o': 2,
+				' ': 1,
+				'w': 1,
+				'r': 1,
+				'd': 1,
+			},
+		},
+		{
+			"你好世界",
+			map[rune]int{
+				'你': 1,
+				'好': 1,
+				'世': 1,
+				'界': 1,
+			},
+		},
+	}
+	for _, c := range cases {
+		got, _ := SymbolCounts(strings.NewReader(c.s))
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("got %v, want %v", got, c.want)
+		}
+	}
+}
+
+func TestXORSingleByte(t *testing.T) {
+	cases := []struct {
+		src  []byte
+		b    byte
+		want []byte
+	}{
+		{
+			[]byte{0, 1, 2, 3, 4, 5},
+			1,
+			[]byte{1, 0, 3, 2, 5, 4},
+		},
+		{
+			[]byte{0, 1, 2, 3, 4, 5},
+			2,
+			[]byte{2, 3, 0, 1, 6, 7},
+		},
+		{
+			[]byte{0, 1, 2, 3, 4, 5},
+			3,
+			[]byte{3, 2, 1, 0, 7, 6},
+		},
+	}
+	dst := make([]byte, 6)
+	for _, c := range cases {
+		XORSingleByte(dst, c.src, c.b)
+		if !bytes.Equal(dst, c.want) {
+			t.Errorf("got %v, want %v", dst, c.want)
 		}
 	}
 }
