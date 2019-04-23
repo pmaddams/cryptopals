4abe1cd312445bf2749041e944f0ff346f802386
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-04-23 11:34:37 -0400

    7

--- set1/07/main.go
+++ set1/07/main.go
@@ -17,6 +17,103 @@ import (
 
 const secret = "YELLOW SUBMARINE"
 
+func main() {
+	var (
+		e  bool
+		fn func(io.Reader, cipher.Block) error
+	)
+	flag.BoolVar(&e, "e", false, "encrypt")
+	flag.Parse()
+	if e {
+		fn = encrypt
+	} else {
+		fn = decrypt
+	}
+	c, err := aes.NewCipher([]byte(secret))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	files := flag.Args()
+	if len(files) == 0 {
+		if err := fn(os.Stdin, c); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+	for _, file := range files {
+		f, err := os.Open(file)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		if err := fn(f, c); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		f.Close()
+	}
+}
+
+// encrypt reads plaintext and prints base64-encoded ciphertext.
+func encrypt(in io.Reader, c cipher.Block) error {
+	buf, err := ioutil.ReadAll(in)
+	if err != nil {
+		return err
+	}
+	buf = PKCS7Pad(buf, c.BlockSize())
+	NewECBEncrypter(c).CryptBlocks(buf, buf)
+	fmt.Println(base64.StdEncoding.EncodeToString(buf))
+
+	return nil
+}
+
+// decrypt reads base64-encoded ciphertext and prints plaintext.
+func decrypt(in io.Reader, c cipher.Block) error {
+	in = base64.NewDecoder(base64.StdEncoding, in)
+	buf, err := ioutil.ReadAll(in)
+	if err != nil {
+		return err
+	}
+	NewECBDecrypter(c).CryptBlocks(buf, buf)
+	buf, err = PKCS7Unpad(buf, c.BlockSize())
+	if err != nil {
+		return err
+	}
+	fmt.Print(string(buf))
+
+	return nil
+}
+
+// PKCS7Pad returns a buffer with PKCS#7 padding added.
+func PKCS7Pad(buf []byte, blockSize int) []byte {
+	if blockSize < 0 || blockSize > 0xff {
+		panic("PKCS7Pad: invalid block size")
+	}
+	// Find the number (and value) of padding bytes.
+	n := blockSize - (len(buf) % blockSize)
+
+	return append(dup(buf), bytes.Repeat([]byte{byte(n)}, n)...)
+}
+
+// PKCS7Unpad returns a buffer with PKCS#7 padding removed.
+func PKCS7Unpad(buf []byte, blockSize int) ([]byte, error) {
+	errInvalidPadding := errors.New("PKCS7Unpad: invalid padding")
+	if len(buf) < blockSize {
+		return nil, errInvalidPadding
+	}
+	// Examine the value of the last byte.
+	b := buf[len(buf)-1]
+	n := len(buf) - int(b)
+	if int(b) == 0 || int(b) > blockSize ||
+		!bytes.Equal(bytes.Repeat([]byte{b}, int(b)), buf[n:]) {
+		return nil, errInvalidPadding
+	}
+	return dup(buf[:n]), nil
+}
+
+// dup returns a copy of a buffer.
+func dup(buf []byte) []byte {
+	return append([]byte{}, buf...)
+}
+
 // ecb represents a generic ECB block mode.
 type ecb struct{ cipher.Block }
 
@@ -55,102 +152,4 @@ func NewECBDecrypter(c cipher.Block) cipher.BlockMode {
 // ecbDecrypter.CryptBlocks decrypts a buffer in ECB mode.
 func (x ecbDecrypter) CryptBlocks(dst, src []byte) {
 	x.cryptBlocks(dst, src, x.Decrypt)
-}
-
-// dup returns a copy of a buffer.
-func dup(buf []byte) []byte {
-	return append([]byte{}, buf...)
-}
-
-// PKCS7Pad returns a buffer with PKCS#7 padding added.
-func PKCS7Pad(buf []byte, blockSize int) []byte {
-	if blockSize < 0 || blockSize > 0xff {
-		panic("PKCS7Pad: invalid block size")
-	}
-	// Find the number (and value) of padding bytes.
-	n := blockSize - (len(buf) % blockSize)
-
-	return append(dup(buf), bytes.Repeat([]byte{byte(n)}, n)...)
-}
-
-// PKCS7Unpad returns a buffer with PKCS#7 padding removed.
-func PKCS7Unpad(buf []byte, blockSize int) ([]byte, error) {
-	errInvalidPadding := errors.New("PKCS7Unpad: invalid padding")
-	if len(buf) < blockSize {
-		return nil, errInvalidPadding
-	}
-	// Examine the value of the last byte.
-	b := buf[len(buf)-1]
-	n := len(buf) - int(b)
-	if int(b) == 0 || int(b) > blockSize ||
-		!bytes.Equal(bytes.Repeat([]byte{b}, int(b)), buf[n:]) {
-		return nil, errInvalidPadding
-	}
-	return dup(buf[:n]), nil
-}
-
-// encryptECB reads plaintext and prints base64-encoded ciphertext.
-func encryptECB(in io.Reader, c cipher.Block) error {
-	buf, err := ioutil.ReadAll(in)
-	if err != nil {
-		return err
-	}
-	buf = PKCS7Pad(buf, c.BlockSize())
-	NewECBEncrypter(c).CryptBlocks(buf, buf)
-	fmt.Println(base64.StdEncoding.EncodeToString(buf))
-
-	return nil
-}
-
-// decryptECB reads base64-encoded ciphertext and prints plaintext.
-func decryptECB(in io.Reader, c cipher.Block) error {
-	in = base64.NewDecoder(base64.StdEncoding, in)
-	buf, err := ioutil.ReadAll(in)
-	if err != nil {
-		return err
-	}
-	NewECBDecrypter(c).CryptBlocks(buf, buf)
-	buf, err = PKCS7Unpad(buf, c.BlockSize())
-	if err != nil {
-		return err
-	}
-	fmt.Print(string(buf))
-
-	return nil
-}
-
-func main() {
-	var (
-		e  bool
-		fn func(io.Reader, cipher.Block) error
-	)
-	flag.BoolVar(&e, "e", false, "encrypt")
-	flag.Parse()
-	if e {
-		fn = encryptECB
-	} else {
-		fn = decryptECB
-	}
-	c, err := aes.NewCipher([]byte(secret))
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
-	files := flag.Args()
-	// If no files are specified, read from standard input.
-	if len(files) == 0 {
-		if err := fn(os.Stdin, c); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	}
-	for _, file := range files {
-		f, err := os.Open(file)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			continue
-		}
-		if err := fn(f, c); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-		f.Close()
-	}
 }
