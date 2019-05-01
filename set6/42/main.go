9be085e6d8b5c7584e4f15f4d61a45fb55fb31f7
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-05-01 19:26:52 -0400

    42

--- set6/42/main.go
+++ set6/42/main.go
@@ -19,6 +19,50 @@ var (
 	three = big.NewInt(3)
 )
 
+func main() {
+	priv, err := RSAGenerateKey(3, 2048)
+	if err != nil {
+		panic(err)
+	}
+	pub := priv.Public()
+	sum, sig, err := forge([]byte("hi mom"), pub, crypto.SHA256)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	if err := RSAVerifyWeak(pub, crypto.SHA256, sum, sig); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	fmt.Println("success")
+}
+
+// forge creates a fake signature for arbitrary data using RSA public exponent 3.
+func forge(buf []byte, pub *RSAPublicKey, id crypto.Hash) ([]byte, []byte, error) {
+	der, err := digestInfo(id)
+	if err != nil {
+		return nil, nil, err
+	}
+	n := size(pub.n)
+	if n < 3+len(der)+id.Size() {
+		return nil, nil, errors.New("forge: insufficient modulus")
+	}
+	h := id.New()
+	h.Write(buf)
+	sum := h.Sum([]byte{})
+
+	tmp := make([]byte, size(pub.n))
+	tmp[1] = 0x01
+	copy(tmp[3:], der)
+	copy(tmp[3+len(der):], sum)
+	for i := 3 + len(der) + len(sum); i < len(tmp); i++ {
+		tmp[i] = 0xff
+	}
+	sig := Cbrt(new(big.Int).SetBytes(tmp)).Bytes()
+
+	return sum, sig, nil
+}
+
 // RSAPublicKey represents the public part of an RSA key pair.
 type RSAPublicKey struct {
 	n *big.Int
@@ -29,11 +73,6 @@ type RSAPublicKey struct {
 type RSAPrivateKey struct {
 	RSAPublicKey
 	d *big.Int
-}
-
-// equal returns true if two arbitrary-precision integers are equal.
-func equal(z1, z2 *big.Int) bool {
-	return z1.Cmp(z2) == 0
 }
 
 // RSAGenerateKey generates a private key.
@@ -78,15 +117,48 @@ func (priv *RSAPrivateKey) Public() *RSAPublicKey {
 	return &priv.RSAPublicKey
 }
 
-// size returns the size of an arbitrary-precision integer in bytes.
-func size(z *big.Int) int {
-	return (z.BitLen() + 7) / 8
+// RSASign returns a PKCS #1 v1.5 signature for a checksum.
+func RSASign(priv *RSAPrivateKey, h crypto.Hash, sum []byte) ([]byte, error) {
+	buf, err := PKCS1v15SignaturePad(h, sum, size(priv.n))
+	if err != nil {
+		return nil, err
+	}
+	if buf, err = RSADecrypt(priv, buf); err != nil {
+		return nil, err
+	}
+	return buf, nil
 }
 
-// copyR copies a source buffer to the right of a destination buffer.
-func copyR(dst, src []byte) int {
-	// Panic if dst is smaller than src.
-	return copy(dst[len(dst)-len(src):], src)
+// RSAVerify returns an error if a checksum does not match its signature.
+func RSAVerify(pub *RSAPublicKey, h crypto.Hash, sum []byte, sig []byte) error {
+	b1, err := PKCS1v15SignaturePad(h, sum, size(pub.n))
+	if err != nil {
+		return err
+	}
+	b2, err := RSAEncrypt(pub, sig)
+	if err != nil {
+		return err
+	}
+	if !bytes.Equal(b1, b2) {
+		return errors.New("RSAVerify: invalid signature")
+	}
+	return nil
+}
+
+// RSAVerifyWeak returns an error if a checksum does not match its signature.
+// It is vulnerable to forgery because it parses the signature incorrectly.
+func RSAVerifyWeak(pub *RSAPublicKey, h crypto.Hash, sum []byte, sig []byte) error {
+	buf, err := RSAEncrypt(pub, sig)
+	if err != nil {
+		return err
+	}
+	if buf, err = PKCS1v15SignatureUnpad(h, buf); err != nil {
+		return err
+	}
+	if len(buf) < len(sum) || !bytes.Equal(buf[:len(sum)], sum) {
+		return errors.New("RSAVerifyWeak: invalid signature")
+	}
+	return nil
 }
 
 // RSAEncrypt takes a public key and plaintext, and returns ciphertext.
@@ -115,24 +187,6 @@ func RSADecrypt(priv *RSAPrivateKey, buf []byte) ([]byte, error) {
 	copyR(res, z.Bytes())
 
 	return res, nil
-}
-
-// digestInfo returns precomputed ASN.1 DER structures for cryptographic hash functions.
-func digestInfo(h crypto.Hash) ([]byte, error) {
-	var buf []byte
-	switch h {
-	case crypto.SHA224:
-		buf = []byte{0x30, 0x2d, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x04, 0x05, 0x00, 0x04, 0x1c}
-	case crypto.SHA256:
-		buf = []byte{0x30, 0x31, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01, 0x05, 0x00, 0x04, 0x20}
-	case crypto.SHA384:
-		buf = []byte{0x30, 0x41, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x02, 0x05, 0x00, 0x04, 0x30}
-	case crypto.SHA512:
-		buf = []byte{0x30, 0x51, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x03, 0x05, 0x00, 0x04, 0x40}
-	default:
-		return nil, errors.New("digestInfo: invalid hash function")
-	}
-	return buf, nil
 }
 
 // PKCS1v15SignaturePad returns a checksum with PKCS #1 v1.5 signature padding added.
@@ -188,48 +242,22 @@ func PKCS1v15SignatureUnpad(h crypto.Hash, buf []byte) ([]byte, error) {
 	return buf, nil
 }
 
-// RSASign returns a PKCS #1 v1.5 signature for a checksum.
-func RSASign(priv *RSAPrivateKey, h crypto.Hash, sum []byte) ([]byte, error) {
-	buf, err := PKCS1v15SignaturePad(h, sum, size(priv.n))
-	if err != nil {
-		return nil, err
-	}
-	if buf, err = RSADecrypt(priv, buf); err != nil {
-		return nil, err
+// digestInfo returns precomputed ASN.1 DER structures for cryptographic hash functions.
+func digestInfo(h crypto.Hash) ([]byte, error) {
+	var buf []byte
+	switch h {
+	case crypto.SHA224:
+		buf = []byte{0x30, 0x2d, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x04, 0x05, 0x00, 0x04, 0x1c}
+	case crypto.SHA256:
+		buf = []byte{0x30, 0x31, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01, 0x05, 0x00, 0x04, 0x20}
+	case crypto.SHA384:
+		buf = []byte{0x30, 0x41, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x02, 0x05, 0x00, 0x04, 0x30}
+	case crypto.SHA512:
+		buf = []byte{0x30, 0x51, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x03, 0x05, 0x00, 0x04, 0x40}
+	default:
+		return nil, errors.New("digestInfo: invalid hash function")
 	}
 	return buf, nil
-}
-
-// RSAVerify returns an error if a checksum does not match its signature.
-func RSAVerify(pub *RSAPublicKey, h crypto.Hash, sum []byte, sig []byte) error {
-	b1, err := PKCS1v15SignaturePad(h, sum, size(pub.n))
-	if err != nil {
-		return err
-	}
-	b2, err := RSAEncrypt(pub, sig)
-	if err != nil {
-		return err
-	}
-	if !bytes.Equal(b1, b2) {
-		return errors.New("RSAVerify: invalid signature")
-	}
-	return nil
-}
-
-// RSAVerifyWeak returns an error if a checksum does not match its signature.
-// It is vulnerable to forgery because it parses the signature incorrectly.
-func RSAVerifyWeak(pub *RSAPublicKey, h crypto.Hash, sum []byte, sig []byte) error {
-	buf, err := RSAEncrypt(pub, sig)
-	if err != nil {
-		return err
-	}
-	if buf, err = PKCS1v15SignatureUnpad(h, buf); err != nil {
-		return err
-	}
-	if len(buf) < len(sum) || !bytes.Equal(buf[:len(sum)], sum) {
-		return errors.New("RSAVerifyWeak: invalid signature")
-	}
-	return nil
 }
 
 // Cbrt returns the cube root of the given integer using successive approximations.
@@ -251,46 +279,18 @@ func Cbrt(z *big.Int) *big.Int {
 	return guess
 }
 
-// forgeSignature creates a fake signature for arbitrary data using RSA public exponent 3.
-func forgeSignature(buf []byte, pub *RSAPublicKey, id crypto.Hash) ([]byte, []byte, error) {
-	der, err := digestInfo(id)
-	if err != nil {
-		return nil, nil, err
-	}
-	n := size(pub.n)
-	if n < 3+len(der)+id.Size() {
-		return nil, nil, errors.New("forgeSignature: insufficient modulus")
-	}
-	h := id.New()
-	h.Write(buf)
-	sum := h.Sum([]byte{})
-
-	tmp := make([]byte, size(pub.n))
-	tmp[1] = 0x01
-	copy(tmp[3:], der)
-	copy(tmp[3+len(der):], sum)
-	for i := 3 + len(der) + len(sum); i < len(tmp); i++ {
-		tmp[i] = 0xff
-	}
-	sig := Cbrt(new(big.Int).SetBytes(tmp)).Bytes()
-
-	return sum, sig, nil
+// copyR copies a source buffer to the right of a destination buffer.
+func copyR(dst, src []byte) int {
+	// Panic if dst is smaller than src.
+	return copy(dst[len(dst)-len(src):], src)
 }
 
-func main() {
-	priv, err := RSAGenerateKey(3, 2048)
-	if err != nil {
-		panic(err)
-	}
-	pub := priv.Public()
-	sum, sig, err := forgeSignature([]byte("hi mom"), pub, crypto.SHA256)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return
-	}
-	if err := RSAVerifyWeak(pub, crypto.SHA256, sum, sig); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return
-	}
-	fmt.Println("success")
+// size returns the size of an arbitrary-precision integer in bytes.
+func size(z *big.Int) int {
+	return (z.BitLen() + 7) / 8
+}
+
+// equal returns true if two arbitrary-precision integers are equal.
+func equal(z1, z2 *big.Int) bool {
+	return z1.Cmp(z2) == 0
 }
