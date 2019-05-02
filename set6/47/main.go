6a4cf8702c9d8fbaa971e70b5048c78b6532817a
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-05-02 18:10:18 -0400

    47

--- set6/47/main.go
+++ set6/47/main.go
@@ -19,6 +19,241 @@ var (
 	three = big.NewInt(3)
 )
 
+func main() {
+	priv, err := RSAGenerateKey(3, 256)
+	if err != nil {
+		panic(err)
+	}
+	pub := &priv.RSAPublicKey
+	oracle := rsaPaddingOracle(priv)
+
+	files := os.Args[1:]
+	if len(files) == 0 {
+		if err := decrypt(os.Stdin, pub, oracle); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		return
+	}
+	for _, file := range files {
+		f, err := os.Open(file)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		if err := decrypt(f, pub, oracle); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		f.Close()
+	}
+}
+
+// decrypt reads lines of text, encrypts them, and prints the decrypted plaintext.
+func decrypt(in io.Reader, pub *RSAPublicKey, oracle func([]byte) bool) error {
+	input := bufio.NewScanner(in)
+	for input.Scan() {
+		ciphertext, err := RSAEncryptPKCS1v15(pub, input.Bytes())
+		if err != nil {
+			return err
+		}
+		x, err := newRSABreaker(pub, oracle, ciphertext)
+		if err != nil {
+			return err
+		}
+		plaintext, err := x.breakOracle()
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(plaintext))
+	}
+	return input.Err()
+}
+
+// rsaPaddingOracle returns an RSA padding oracle.
+func rsaPaddingOracle(priv *RSAPrivateKey) func([]byte) bool {
+	return func(ciphertext []byte) bool {
+		buf, err := RSADecrypt(priv, ciphertext)
+		if err != nil {
+			panic(err)
+		}
+		return buf[0] == 0x00 && buf[1] == 0x02
+	}
+}
+
+// rsaBreaker contains state for attacking the PKCS #1 v1.5 padding oracle.
+type rsaBreaker struct {
+	oracle func([]byte) bool
+	e      *big.Int
+	n      *big.Int
+	c      *big.Int
+	twoB   *big.Int
+	threeB *big.Int
+	s      *big.Int
+	ivals  []interval
+}
+
+// interval represents a range of possible plaintexts.
+type interval struct {
+	lo *big.Int
+	hi *big.Int
+}
+
+// newRSABreaker takes a public key, oracle, and ciphertext, and returns a breaker.
+func newRSABreaker(pub *RSAPublicKey, oracle func([]byte) bool, ciphertext []byte) (*rsaBreaker, error) {
+	x := new(rsaBreaker)
+	x.oracle = oracle
+
+	x.e = new(big.Int).Set(pub.e)
+	x.n = new(big.Int).Set(pub.n)
+	x.c = new(big.Int).SetBytes(ciphertext)
+
+	z := big.NewInt(int64(8 * (size(x.n) - 2)))
+	b := z.Exp(two, z, nil)
+	x.twoB = new(big.Int).Mul(two, b)
+	x.threeB = new(big.Int).Mul(three, b)
+
+	sMin := z.Div(x.n, x.threeB)
+	s, err := x.findS(sMin, x.n)
+	if err != nil {
+		return nil, err
+	} else if s == nil {
+		return nil, errors.New("newRSABreaker: nothing found")
+	}
+	x.s = s
+	x.ivals = append(x.ivals, interval{
+		new(big.Int).Set(x.twoB),
+		new(big.Int).Sub(x.threeB, one),
+	})
+	return x, nil
+}
+
+// breakOracle breaks the padding oracle and returns the plaintext.
+func (x *rsaBreaker) breakOracle() ([]byte, error) {
+	for {
+		x.generateIntervals()
+		switch len(x.ivals) {
+		case 0:
+			return nil, errors.New("breakOracle: no intervals")
+		case 1:
+			m := x.ivals[0]
+			if equal(m.lo, m.hi) {
+				buf := make([]byte, size(x.n))
+				copyR(buf, m.lo.Bytes())
+
+				plaintext, err := PKCS1v15CryptUnpad(buf)
+				if err != nil {
+					return nil, err
+				}
+				return plaintext, nil
+			}
+			x.searchOne(m)
+		default:
+			x.searchMany()
+		}
+	}
+}
+
+// searchMany searches for the next "s" value for multiple intervals.
+func (x *rsaBreaker) searchMany() error {
+	sMin := new(big.Int).Add(x.s, one)
+	s, err := x.findS(sMin, x.n)
+	if err != nil {
+		return err
+	} else if s == nil {
+		return errors.New("searchMany: nothing found")
+	}
+	x.s = s
+	return nil
+}
+
+// searchOne searches for the next "s" value for a single interval.
+func (x *rsaBreaker) searchOne(m interval) error {
+	r := new(big.Int).Mul(m.hi, x.s)
+	r.Sub(r, x.twoB)
+	r.Mul(two, r)
+	r.Div(r, x.n)
+
+	sMin, sMax := new(big.Int), new(big.Int)
+	for {
+		sMin.Mul(r, x.n)
+		sMin.Add(sMin, x.twoB)
+		sMin.Div(sMin, m.hi)
+
+		sMax.Mul(r, x.n)
+		sMax.Add(sMax, x.threeB)
+		sMax.Div(sMax, m.lo)
+
+		s, err := x.findS(sMin, sMax)
+		if err != nil {
+			return err
+		} else if s != nil {
+			x.s = s
+			return nil
+		}
+		r.Add(r, one)
+	}
+}
+
+// generateIntervals generates the next set of intervals.
+func (x *rsaBreaker) generateIntervals() {
+	var ivals []interval
+	for _, m := range x.ivals {
+		for r, rMax := x.intervalRValues(m); r.Cmp(rMax) <= 0; r.Add(r, one) {
+			lo, hi := new(big.Int), new(big.Int)
+
+			lo.Mul(r, x.n)
+			lo.Add(lo, x.twoB)
+			ceilingDiv(lo, lo, x.s)
+			if lo.Cmp(m.lo) < 0 {
+				lo.Set(m.lo)
+			}
+			hi.Mul(r, x.n)
+			hi.Add(hi, x.threeB)
+			hi.Sub(hi, one)
+			hi.Div(hi, x.s)
+			if hi.Cmp(m.hi) > 0 {
+				hi.Set(m.hi)
+			}
+			ivals = append(ivals, interval{lo, hi})
+		}
+	}
+	x.ivals = ivals
+}
+
+// intervalRValues returns the bounds for generating the next set of intervals.
+func (x *rsaBreaker) intervalRValues(m interval) (*big.Int, *big.Int) {
+	r := new(big.Int).Mul(m.lo, x.s)
+	r.Sub(r, x.threeB)
+	r.Add(r, one)
+	ceilingDiv(r, r, x.n)
+
+	rMax := new(big.Int).Mul(m.hi, x.s)
+	rMax.Sub(rMax, x.twoB)
+	rMax.Div(rMax, x.n)
+
+	return r, rMax
+}
+
+// findS finds the smallest multiple of the ciphertext that generates valid padding.
+func (x *rsaBreaker) findS(sMin, sMax *big.Int) (*big.Int, error) {
+	if sMin.Cmp(sMax) > 0 {
+		return nil, errors.New("findS: invalid range")
+	}
+	s := new(big.Int).Set(sMin)
+	cPrime := new(big.Int)
+	for {
+		if s.Cmp(sMax) > 0 {
+			return nil, nil
+		}
+		cPrime.Exp(s, x.e, x.n)
+		cPrime.Mul(cPrime, x.c)
+		cPrime.Mod(cPrime, x.n)
+		if x.oracle(cPrime.Bytes()) {
+			return s, nil
+		}
+		s.Add(s, one)
+	}
+}
+
 // RSAPublicKey represents the public part of an RSA key pair.
 type RSAPublicKey struct {
 	n *big.Int
@@ -29,11 +264,6 @@ type RSAPublicKey struct {
 type RSAPrivateKey struct {
 	RSAPublicKey
 	d *big.Int
-}
-
-// equal returns true if two arbitrary-precision integers are equal.
-func equal(z1, z2 *big.Int) bool {
-	return z1.Cmp(z2) == 0
 }
 
 // RSAGenerateKey generates a private key.
@@ -78,15 +308,22 @@ func (priv *RSAPrivateKey) Public() *RSAPublicKey {
 	return &priv.RSAPublicKey
 }
 
-// size returns the size of an arbitrary-precision integer in bytes.
-func size(z *big.Int) int {
-	return (z.BitLen() + 7) / 8
+// RSAEncryptPKCS1v15 takes a public key and plaintext, and returns PKCS #1 v1.5 padded ciphertext.
+func RSAEncryptPKCS1v15(pub *RSAPublicKey, buf []byte) ([]byte, error) {
+	buf, err := PKCS1v15CryptPad(buf, size(pub.n))
+	if err != nil {
+		return nil, err
+	}
+	return RSAEncrypt(pub, buf)
 }
 
-// copyR copies a source buffer to the right of a destination buffer.
-func copyR(dst, src []byte) int {
-	// Panic if dst is smaller than src.
-	return copy(dst[len(dst)-len(src):], src)
+// RSADecryptPKCS1v15 takes a private key and PKCS #1 v1.5 padded ciphertext, and returns plaintext.
+func RSADecryptPKCS1v15(priv *RSAPrivateKey, buf []byte) ([]byte, error) {
+	buf, err := RSADecrypt(priv, buf)
+	if err != nil {
+		return nil, err
+	}
+	return PKCS1v15CryptUnpad(buf)
 }
 
 // RSAEncrypt takes a public key and plaintext, and returns ciphertext.
@@ -115,26 +352,6 @@ func RSADecrypt(priv *RSAPrivateKey, buf []byte) ([]byte, error) {
 	copyR(res, z.Bytes())
 
 	return res, nil
-}
-
-// RandomBytes returns a random buffer of the desired length.
-func RandomBytes(n int) []byte {
-	buf := make([]byte, n)
-	if _, err := rand.Read(buf); err != nil {
-		panic(err)
-	}
-	return buf
-}
-
-// randomNonzeroBytes returns a random buffer of the desired length containing no zero bytes.
-func randomNonzeroBytes(n int) []byte {
-	buf := RandomBytes(n)
-	for i := range buf {
-		for buf[i] == 0 {
-			buf[i] = RandomBytes(1)[0]
-		}
-	}
-	return buf
 }
 
 // PKCS1v15CryptPad returns a checksum with PKCS #1 v1.5 encryption padding added.
@@ -172,101 +389,24 @@ func PKCS1v15CryptUnpad(buf []byte) ([]byte, error) {
 	return buf, nil
 }
 
-// RSAEncryptPKCS1v15 takes a public key and plaintext, and returns PKCS #1 v1.5 padded ciphertext.
-func RSAEncryptPKCS1v15(pub *RSAPublicKey, buf []byte) ([]byte, error) {
-	buf, err := PKCS1v15CryptPad(buf, size(pub.n))
-	if err != nil {
-		return nil, err
-	}
-	return RSAEncrypt(pub, buf)
-}
-
-// RSADecryptPKCS1v15 takes a private key and PKCS #1 v1.5 padded ciphertext, and returns plaintext.
-func RSADecryptPKCS1v15(priv *RSAPrivateKey, buf []byte) ([]byte, error) {
-	buf, err := RSADecrypt(priv, buf)
-	if err != nil {
-		return nil, err
-	}
-	return PKCS1v15CryptUnpad(buf)
-}
-
-// rsaPaddingOracle returns an RSA padding oracle.
-func rsaPaddingOracle(priv *RSAPrivateKey) func([]byte) bool {
-	return func(ciphertext []byte) bool {
-		buf, err := RSADecrypt(priv, ciphertext)
-		if err != nil {
-			panic(err)
+// randomNonzeroBytes returns a random buffer of the desired length containing no zero bytes.
+func randomNonzeroBytes(n int) []byte {
+	buf := RandomBytes(n)
+	for i := range buf {
+		for buf[i] == 0 {
+			buf[i] = RandomBytes(1)[0]
 		}
-		return buf[0] == 0x00 && buf[1] == 0x02
 	}
+	return buf
 }
 
-// interval represents a range of possible plaintexts.
-type interval struct {
-	lo *big.Int
-	hi *big.Int
-}
-
-// rsaBreaker contains state for attacking the PKCS #1 v1.5 padding oracle.
-type rsaBreaker struct {
-	oracle func([]byte) bool
-	e      *big.Int
-	n      *big.Int
-	c      *big.Int
-	twoB   *big.Int
-	threeB *big.Int
-	s      *big.Int
-	ivals  []interval
-}
-
-// newRSABreaker takes a public key, oracle, and ciphertext, and returns a breaker.
-func newRSABreaker(pub *RSAPublicKey, oracle func([]byte) bool, ciphertext []byte) (*rsaBreaker, error) {
-	x := new(rsaBreaker)
-	x.oracle = oracle
-
-	x.e = new(big.Int).Set(pub.e)
-	x.n = new(big.Int).Set(pub.n)
-	x.c = new(big.Int).SetBytes(ciphertext)
-
-	z := big.NewInt(int64(8 * (size(x.n) - 2)))
-	b := z.Exp(two, z, nil)
-	x.twoB = new(big.Int).Mul(two, b)
-	x.threeB = new(big.Int).Mul(three, b)
-
-	sMin := z.Div(x.n, x.threeB)
-	s, err := x.findS(sMin, x.n)
-	if err != nil {
-		return nil, err
-	} else if s == nil {
-		return nil, errors.New("newRSABreaker: nothing found")
+// RandomBytes returns a random buffer of the desired length.
+func RandomBytes(n int) []byte {
+	buf := make([]byte, n)
+	if _, err := rand.Read(buf); err != nil {
+		panic(err)
 	}
-	x.s = s
-	x.ivals = append(x.ivals, interval{
-		new(big.Int).Set(x.twoB),
-		new(big.Int).Sub(x.threeB, one),
-	})
-	return x, nil
-}
-
-// findS finds the smallest multiple of the ciphertext that generates valid padding.
-func (x *rsaBreaker) findS(sMin, sMax *big.Int) (*big.Int, error) {
-	if sMin.Cmp(sMax) > 0 {
-		return nil, errors.New("findS: invalid range")
-	}
-	s := new(big.Int).Set(sMin)
-	cPrime := new(big.Int)
-	for {
-		if s.Cmp(sMax) > 0 {
-			return nil, nil
-		}
-		cPrime.Exp(s, x.e, x.n)
-		cPrime.Mul(cPrime, x.c)
-		cPrime.Mod(cPrime, x.n)
-		if x.oracle(cPrime.Bytes()) {
-			return s, nil
-		}
-		s.Add(s, one)
-	}
+	return buf
 }
 
 // ceilingDiv performs ceiling division of z1 by z2.
@@ -279,158 +419,18 @@ func ceilingDiv(res, z1, z2 *big.Int) *big.Int {
 	return res
 }
 
-// intervalRValues returns the bounds for generating the next set of intervals.
-func (x *rsaBreaker) intervalRValues(m interval) (*big.Int, *big.Int) {
-	r := new(big.Int).Mul(m.lo, x.s)
-	r.Sub(r, x.threeB)
-	r.Add(r, one)
-	ceilingDiv(r, r, x.n)
-
-	rMax := new(big.Int).Mul(m.hi, x.s)
-	rMax.Sub(rMax, x.twoB)
-	rMax.Div(rMax, x.n)
-
-	return r, rMax
+// copyR copies a source buffer to the right of a destination buffer.
+func copyR(dst, src []byte) int {
+	// Panic if dst is smaller than src.
+	return copy(dst[len(dst)-len(src):], src)
 }
 
-// generateIntervals generates the next set of intervals.
-func (x *rsaBreaker) generateIntervals() {
-	var ivals []interval
-	for _, m := range x.ivals {
-		for r, rMax := x.intervalRValues(m); r.Cmp(rMax) <= 0; r.Add(r, one) {
-			lo, hi := new(big.Int), new(big.Int)
-
-			lo.Mul(r, x.n)
-			lo.Add(lo, x.twoB)
-			ceilingDiv(lo, lo, x.s)
-			if lo.Cmp(m.lo) < 0 {
-				lo.Set(m.lo)
-			}
-			hi.Mul(r, x.n)
-			hi.Add(hi, x.threeB)
-			hi.Sub(hi, one)
-			hi.Div(hi, x.s)
-			if hi.Cmp(m.hi) > 0 {
-				hi.Set(m.hi)
-			}
-			ivals = append(ivals, interval{lo, hi})
-		}
-	}
-	x.ivals = ivals
+// size returns the size of an arbitrary-precision integer in bytes.
+func size(z *big.Int) int {
+	return (z.BitLen() + 7) / 8
 }
 
-// searchOne searches for the next "s" value for a single interval.
-func (x *rsaBreaker) searchOne(m interval) error {
-	r := new(big.Int).Mul(m.hi, x.s)
-	r.Sub(r, x.twoB)
-	r.Mul(two, r)
-	r.Div(r, x.n)
-
-	sMin, sMax := new(big.Int), new(big.Int)
-	for {
-		sMin.Mul(r, x.n)
-		sMin.Add(sMin, x.twoB)
-		sMin.Div(sMin, m.hi)
-
-		sMax.Mul(r, x.n)
-		sMax.Add(sMax, x.threeB)
-		sMax.Div(sMax, m.lo)
-
-		s, err := x.findS(sMin, sMax)
-		if err != nil {
-			return err
-		} else if s != nil {
-			x.s = s
-			return nil
-		}
-		r.Add(r, one)
-	}
-}
-
-// searchMany searches for the next "s" value for multiple intervals.
-func (x *rsaBreaker) searchMany() error {
-	sMin := new(big.Int).Add(x.s, one)
-	s, err := x.findS(sMin, x.n)
-	if err != nil {
-		return err
-	} else if s == nil {
-		return errors.New("searchMany: nothing found")
-	}
-	x.s = s
-	return nil
-}
-
-// breakOracle breaks the padding oracle and returns the plaintext.
-func (x *rsaBreaker) breakOracle() ([]byte, error) {
-	for {
-		x.generateIntervals()
-		switch len(x.ivals) {
-		case 0:
-			return nil, errors.New("breakOracle: no intervals")
-		case 1:
-			m := x.ivals[0]
-			if equal(m.lo, m.hi) {
-				buf := make([]byte, size(x.n))
-				copyR(buf, m.lo.Bytes())
-
-				plaintext, err := PKCS1v15CryptUnpad(buf)
-				if err != nil {
-					return nil, err
-				}
-				return plaintext, nil
-			}
-			x.searchOne(m)
-		default:
-			x.searchMany()
-		}
-	}
-}
-
-// decryptRSA reads lines of text, encrypts them, and prints the decrypted plaintext.
-func decryptRSA(in io.Reader, pub *RSAPublicKey, oracle func([]byte) bool) error {
-	input := bufio.NewScanner(in)
-	for input.Scan() {
-		ciphertext, err := RSAEncryptPKCS1v15(pub, input.Bytes())
-		if err != nil {
-			return err
-		}
-		x, err := newRSABreaker(pub, oracle, ciphertext)
-		if err != nil {
-			return err
-		}
-		plaintext, err := x.breakOracle()
-		if err != nil {
-			return err
-		}
-		fmt.Println(string(plaintext))
-	}
-	return input.Err()
-}
-
-func main() {
-	priv, err := RSAGenerateKey(3, 256)
-	if err != nil {
-		panic(err)
-	}
-	pub := &priv.RSAPublicKey
-	oracle := rsaPaddingOracle(priv)
-
-	files := os.Args[1:]
-	if len(files) == 0 {
-		if err := decryptRSA(os.Stdin, pub, oracle); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-		return
-	}
-	for _, file := range files {
-		f, err := os.Open(file)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			continue
-		}
-		if err := decryptRSA(f, pub, oracle); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-		f.Close()
-	}
+// equal returns true if two arbitrary-precision integers are equal.
+func equal(z1, z2 *big.Int) bool {
+	return z1.Cmp(z2) == 0
 }
--- set6/47/main_test.go
+++ set6/47/main_test.go
@@ -10,6 +10,37 @@ import (
 
 func init() { weak.Seed(time.Now().UnixNano()) }
 
+func TestRSACryptPKCS1v15(t *testing.T) {
+	const (
+		exponent = 3
+		bits     = 256
+	)
+	buf := make([]byte, 16)
+	for i := 0; i < 5; i++ {
+		priv, err := RSAGenerateKey(exponent, bits)
+		if err != nil {
+			t.Error(err)
+		}
+		if n := priv.n.BitLen(); n != bits {
+			t.Errorf("got bit size %v, want %v", n, bits)
+		}
+		weak.Read(buf)
+		ciphertext, err := RSAEncryptPKCS1v15(priv.Public(), buf)
+		if err != nil {
+			t.Error(err)
+		}
+		plaintext, err := RSADecryptPKCS1v15(priv, ciphertext)
+		if err != nil {
+			t.Error(err)
+		}
+		want := new(big.Int).SetBytes(buf)
+		got := new(big.Int).SetBytes(plaintext)
+		if !equal(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+}
+
 func TestRSA(t *testing.T) {
 	const (
 		exponent = 3
@@ -64,33 +95,14 @@ func TestPKCS1v15CryptPadUnpad(t *testing.T) {
 	}
 }
 
-func TestRSACryptPKCS1v15(t *testing.T) {
-	const (
-		exponent = 3
-		bits     = 256
-	)
-	buf := make([]byte, 16)
+func TestRandomBytes(t *testing.T) {
+	var bufs [][]byte
 	for i := 0; i < 5; i++ {
-		priv, err := RSAGenerateKey(exponent, bits)
-		if err != nil {
-			t.Error(err)
-		}
-		if n := priv.n.BitLen(); n != bits {
-			t.Errorf("got bit size %v, want %v", n, bits)
-		}
-		weak.Read(buf)
-		ciphertext, err := RSAEncryptPKCS1v15(priv.Public(), buf)
-		if err != nil {
-			t.Error(err)
-		}
-		plaintext, err := RSADecryptPKCS1v15(priv, ciphertext)
-		if err != nil {
-			t.Error(err)
-		}
-		want := new(big.Int).SetBytes(buf)
-		got := new(big.Int).SetBytes(plaintext)
-		if !equal(got, want) {
-			t.Errorf("got %v, want %v", got, want)
+		bufs = append(bufs, RandomBytes(16))
+		for j := 0; j < i; j++ {
+			if bytes.Equal(bufs[i], bufs[j]) {
+				t.Errorf("identical buffers %v and %v", bufs[i], bufs[j])
+			}
 		}
 	}
 }
