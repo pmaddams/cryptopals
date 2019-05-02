dd70dc8aa842fd0908895998ab6ec15dba5db73e
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-05-02 17:41:44 -0400

    46

--- set6/46/main.go
+++ set6/46/main.go
@@ -21,6 +21,105 @@ var (
 	two = big.NewInt(2)
 )
 
+func main() {
+	fmt.Print("generating RSA key...")
+	priv, err := RSAGenerateKey(rsaExponent, 1024)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("done.")
+	oracle := parityOracle(priv)
+	x := newParityBreaker(priv.Public(), oracle)
+
+	files := os.Args[1:]
+	if len(files) == 0 {
+		if err := decrypt(os.Stdin, x); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		return
+	}
+	for _, file := range files {
+		f, err := os.Open(file)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		if err := decrypt(f, x); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		f.Close()
+	}
+}
+
+// decrypt reads lines of base64-encoded input, encrypts them, and prints the decryption process.
+func decrypt(in io.Reader, x *parityBreaker) error {
+	input := bufio.NewScanner(in)
+	for input.Scan() {
+		buf, err := base64.StdEncoding.DecodeString(input.Text())
+		if err != nil {
+			return err
+		}
+		ciphertext, err := RSAEncrypt(x.RSAPublicKey, buf)
+		if err != nil {
+			return err
+		}
+		plaintext, err := x.breakOracle(ciphertext)
+		if err != nil {
+			return err
+		}
+		printValid(plaintext)
+	}
+	return input.Err()
+}
+
+// parityOracle takes an RSA private key and returns a parity oracle.
+func parityOracle(priv *RSAPrivateKey) func([]byte) (bool, error) {
+	return func(ciphertext []byte) (bool, error) {
+		plaintext, err := RSADecrypt(priv, ciphertext)
+		if err != nil {
+			return false, err
+		}
+		return trailingZero(plaintext), nil
+	}
+}
+
+// parityBreaker contains state for attacking the parity oracle.
+type parityBreaker struct {
+	*RSAPublicKey
+	oracle func([]byte) (bool, error)
+}
+
+// newParityBreaker takes a public key and parity oracle, and returns a breaker.
+func newParityBreaker(pub *RSAPublicKey, oracle func([]byte) (bool, error)) *parityBreaker {
+	return &parityBreaker{pub, oracle}
+}
+
+// breakOracle breaks the parity oracle and returns the plaintext.
+func (x *parityBreaker) breakOracle(ciphertext []byte) ([]byte, error) {
+	c := new(big.Int).SetBytes(ciphertext)
+	p := new(big.Int)
+	encryptedTwo := new(big.Int).Exp(two, x.e, x.n)
+
+	lo, hi := big.NewInt(0), new(big.Int).Sub(x.n, one)
+	for !equal(lo, hi) {
+		c.Mul(c, encryptedTwo)
+		c.Mod(c, x.n)
+		even, err := x.oracle(c.Bytes())
+		if err != nil {
+			return nil, err
+		}
+		p.Add(lo, hi)
+		p.Div(p, two)
+		if even {
+			hi.Set(p)
+		} else {
+			lo.Add(p, one)
+		}
+		printValid(p.Bytes())
+	}
+	return p.Bytes(), nil
+}
+
 // RSAPublicKey represents the public part of an RSA key pair.
 type RSAPublicKey struct {
 	n *big.Int
@@ -31,11 +130,6 @@ type RSAPublicKey struct {
 type RSAPrivateKey struct {
 	RSAPublicKey
 	d *big.Int
-}
-
-// equal returns true if two arbitrary-precision integers are equal.
-func equal(z1, z2 *big.Int) bool {
-	return z1.Cmp(z2) == 0
 }
 
 // RSAGenerateKey generates a private key.
@@ -80,17 +174,6 @@ func (priv *RSAPrivateKey) Public() *RSAPublicKey {
 	return &priv.RSAPublicKey
 }
 
-// size returns the size of an arbitrary-precision integer in bytes.
-func size(z *big.Int) int {
-	return (z.BitLen() + 7) / 8
-}
-
-// copyR copies a source buffer to the right of a destination buffer.
-func copyR(dst, src []byte) int {
-	// Panic if dst is smaller than src.
-	return copy(dst[len(dst)-len(src):], src)
-}
-
 // RSAEncrypt takes a public key and plaintext, and returns ciphertext.
 func RSAEncrypt(pub *RSAPublicKey, buf []byte) ([]byte, error) {
 	z := new(big.Int).SetBytes(buf)
@@ -119,35 +202,8 @@ func RSADecrypt(priv *RSAPrivateKey, buf []byte) ([]byte, error) {
 	return res, nil
 }
 
-// trailingZero returns true if the buffer has a trailing zero bit.
-func trailingZero(buf []byte) bool {
-	return buf[len(buf)-1]&1 == 0
-}
-
-// parityOracle takes an RSA private key and returns a parity oracle.
-func parityOracle(priv *RSAPrivateKey) func([]byte) (bool, error) {
-	return func(ciphertext []byte) (bool, error) {
-		plaintext, err := RSADecrypt(priv, ciphertext)
-		if err != nil {
-			return false, err
-		}
-		return trailingZero(plaintext), nil
-	}
-}
-
-// parityBreaker contains state for attacking the parity oracle.
-type parityBreaker struct {
-	*RSAPublicKey
-	oracle func([]byte) (bool, error)
-}
-
-// newParityBreaker takes a public key and parity oracle, and returns a breaker.
-func newParityBreaker(pub *RSAPublicKey, oracle func([]byte) (bool, error)) *parityBreaker {
-	return &parityBreaker{pub, oracle}
-}
-
-// printRunes prints printable runes in a buffer.
-func printRunes(buf []byte) {
+// printValid prints valid symbols in a buffer.
+func printValid(buf []byte) {
 	for _, r := range string(buf) {
 		if unicode.IsPrint(r) {
 			fmt.Print(string(r))
@@ -156,79 +212,23 @@ func printRunes(buf []byte) {
 	fmt.Println()
 }
 
-// breakOracle breaks the parity oracle and returns the plaintext.
-func (x *parityBreaker) breakOracle(ciphertext []byte) ([]byte, error) {
-	c := new(big.Int).SetBytes(ciphertext)
-	p := new(big.Int)
-	encryptedTwo := new(big.Int).Exp(two, x.e, x.n)
-
-	lo, hi := big.NewInt(0), new(big.Int).Sub(x.n, one)
-	for !equal(lo, hi) {
-		c.Mul(c, encryptedTwo)
-		c.Mod(c, x.n)
-		even, err := x.oracle(c.Bytes())
-		if err != nil {
-			return nil, err
-		}
-		p.Add(lo, hi)
-		p.Div(p, two)
-		if even {
-			hi.Set(p)
-		} else {
-			lo.Add(p, one)
-		}
-		printRunes(p.Bytes())
-	}
-	return p.Bytes(), nil
+// copyR copies a source buffer to the right of a destination buffer.
+func copyR(dst, src []byte) int {
+	// Panic if dst is smaller than src.
+	return copy(dst[len(dst)-len(src):], src)
 }
 
-// decryptHollywoodStyle reads lines of base64-encoded input, encrypts them, and prints them "Hollywood style".
-func decryptHollywoodStyle(in io.Reader, x *parityBreaker) error {
-	input := bufio.NewScanner(in)
-	for input.Scan() {
-		buf, err := base64.StdEncoding.DecodeString(input.Text())
-		if err != nil {
-			return err
-		}
-		ciphertext, err := RSAEncrypt(x.RSAPublicKey, buf)
-		if err != nil {
-			return err
-		}
-		plaintext, err := x.breakOracle(ciphertext)
-		if err != nil {
-			return err
-		}
-		printRunes(plaintext)
-	}
-	return input.Err()
+// trailingZero returns true if the buffer has a trailing zero bit.
+func trailingZero(buf []byte) bool {
+	return buf[len(buf)-1]&1 == 0
 }
 
-func main() {
-	fmt.Print("generating RSA key...")
-	priv, err := RSAGenerateKey(rsaExponent, 1024)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("done.")
-	oracle := parityOracle(priv)
-	x := newParityBreaker(priv.Public(), oracle)
+// size returns the size of an arbitrary-precision integer in bytes.
+func size(z *big.Int) int {
+	return (z.BitLen() + 7) / 8
+}
 
-	files := os.Args[1:]
-	if len(files) == 0 {
-		if err := decryptHollywoodStyle(os.Stdin, x); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-		return
-	}
-	for _, file := range files {
-		f, err := os.Open(file)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			continue
-		}
-		if err := decryptHollywoodStyle(f, x); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-		f.Close()
-	}
+// equal returns true if two arbitrary-precision integers are equal.
+func equal(z1, z2 *big.Int) bool {
+	return z1.Cmp(z2) == 0
 }
